af485e583a06409583e95c8184c31a580da0c8c7
szondyb <szondy.borbala@gmail.com>
2017-03-15 18:34:16 +0100

    Odeint has been inserted as ode solver, all tests have been passed.

--- renate-od_crm_solver/ode_solver.py
+++ renate-od_crm_solver/ode_solver.py
@@ -1,9 +1,20 @@
 import numpy
+from scipy.integrate import odeint
+
 
 
 class ode_solver:
-    def set_up_equation(variable_vector, coefficient_matrix):
+    def set_up_equation(variable_vector, calculation_point, coefficient_matrix):
         derivative_vector = numpy.dot(variable_vector, coefficient_matrix)
         return derivative_vector
+
+    def calculate_solution(set_up_equation,initial_condition,distances,coefficient_matrix):
+        solution=odeint(set_up_equation,initial_condition,distances,args=(coefficient_matrix,))
+        return solution
+
+
+
+
+
 
 
--- renate-od_crm_solver/test_ode_solver.py
+++ renate-od_crm_solver/test_ode_solver.py
@@ -9,7 +9,24 @@ class TestOde_solver(TestCase):
     c = numpy.zeros((2, 2))
     c[0, 1] = 2
     c[0, 0] = 1
+    initial_condition = numpy.zeros(4)
+    initial_condition[0] = 1
+    interval = 100
+    number_of_points = 5
+    distances = numpy.linspace(0, interval, number_of_points)
+    coefficient_matrix = numpy.zeros((4, 4))
+    for i in range(4):
+        coefficient_matrix[i, i] = 1
+        coefficient_matrix[i, i - 1] = 0.7
 
     def test_set_up_equation(self):
-        s = ode_solver.set_up_equation(variable_vector=self.pop, coefficient_matrix=self.c)
-        self.assertEqual(2, s.size)
+        s = ode_solver.set_up_equation(variable_vector=self.initial_condition, calculation_point=self.distances,coefficient_matrix=self.coefficient_matrix)
+        self.assertEqual(4, s.size)
+
+    def test_calculate_solution(self):
+        s = ode_solver.calculate_solution(ode_solver.set_up_equation,self.initial_condition,self.distances,self.coefficient_matrix)
+        self.assertEqual(s.size,self.number_of_points*self.initial_condition.size)
+
+
+
+
