02ad7489a05787103dfbd7c12d5c429f00878159
szondyb <szondy.borbala@gmail.com>
2017-03-16 09:54:24 +0100

    #5 ode_solver extended for changing parameter odes.

--- renate-od_crm_solver/ode_solver.py
+++ renate-od_crm_solver/ode_solver.py
@@ -6,13 +6,19 @@ from scipy.integrate import odeint
 
 class ode_solver:
     def set_up_equation(variable_vector, calculation_point, coefficient_matrix):
-        derivative_vector = numpy.dot(variable_vector, coefficient_matrix)
+        if coefficient_matrix.ndim==3:
+            derivative_vector = numpy.dot(variable_vector, coefficient_matrix[:, :, calculation_point])
+        elif coefficient_matrix.ndim==2:
+            derivative_vector = numpy.dot(variable_vector, coefficient_matrix)
         return derivative_vector
+
 
     def calculate_solution(set_up_equation,initial_condition,steps,\
                            coefficient_matrix):
         solution=odeint(set_up_equation,initial_condition,t=steps,args=(coefficient_matrix,))
         return solution
+
+
 
 
 
