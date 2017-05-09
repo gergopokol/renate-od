ff5a74cd174d8cebfd469478af7bc0c9e83e1d4b
szondyb <szondy.borbala@gmail.com>
2017-05-09 19:05:35 +0200

    #5 PyCharm recommended refactoring done

--- crm_solver/inputs.py
+++ crm_solver/inputs.py
@@ -7,37 +7,12 @@ class Inputs:
     step_number = 100
     beam_energy = 60
     beam_species = 'Li'
-    electron_temperature = 3e3
-    proton_temperature = 2e2
-    density = 6e19
+    electron_temperature = numpy.linspace(1,6,step_number)*1e3
+    proton_temperature = numpy.linspace(1,6,step_number)*1e3
+    density = numpy.linspace(1,6,step_number)*1e20
 
     number_of_levels = len(initial_condition)
     steps = numpy.linspace(0, step_interval, step_number)
 
 
-class Inputs1:
-    initial_condition = numpy.array([1, 0, 0, 0, 0, 0, 0, 0, 0])
-    step_interval = 0.1
-    step_number = 100
-    beam_energy = 60
-    beam_species = 'Li'
-    electron_temperature = 3e3
-    proton_temperature = 3e2
-    density = 6e19
 
-    number_of_levels = len(initial_condition)
-    steps = numpy.linspace(0, step_interval, step_number)
-
-
-class Inputs2:
-    initial_condition = numpy.array([1, 0, 0, 0, 0, 0, 0, 0, 0])
-    step_interval = 0.1
-    step_number = 30
-    beam_energy = 60
-    beam_species = 'Li'
-    electron_temperature = 3e3
-    proton_temperature = 3e2
-    density = 6e19
-
-    number_of_levels = len(initial_condition)
-    steps = numpy.linspace(0, step_interval, step_number)
--- crm_solver/rates.py
+++ crm_solver/rates.py
@@ -3,23 +3,25 @@ import os
 import numpy
 import math
 from scipy.interpolate import interp1d
-from crm_solver.inputs import Inputs1
+from crm_solver.inputs import Inputs
 
 
 class Rates:
     # Get rate coefficients from hdf5 files:
-    def __init__(self, inputs=Inputs1()):
-        self.inputs=inputs
-        rate_coefficients=self.setup_rate_coeff_arrays()
-        temperature_array=rate_coefficients[0]
-        electron_neutral_collisions_array=rate_coefficients[1]
-        proton_neutral_collisions_array=rate_coefficients[2]
-        electron_loss_collisions_array=rate_coefficients[4]
-        einstein_coeffs_array=rate_coefficients[5]
+    def __init__(self, inputs=Inputs()):
+        self.inputs = inputs
+        rate_coefficients = self.setup_rate_coeff_arrays()
+        temperature_array = rate_coefficients[0]
+        electron_neutral_collisions_array = rate_coefficients[1]
+        proton_neutral_collisions_array = rate_coefficients[2]
+        electron_loss_collisions_array = rate_coefficients[4]
+        einstein_coeffs_array = rate_coefficients[5]
 
         # Interpolate rate coeffs to new grid:
-        electron_neutral_collisions_array_new = numpy.zeros((inputs.number_of_levels, inputs.number_of_levels, len(inputs.steps)))
-        proton_neutral_collisions_array_new = numpy.zeros((inputs.number_of_levels, inputs.number_of_levels, len(inputs.steps)))
+        electron_neutral_collisions_array_new = numpy.zeros((inputs.number_of_levels, inputs.number_of_levels,
+                                                             len(inputs.steps)))
+        proton_neutral_collisions_array_new = numpy.zeros((inputs.number_of_levels, inputs.number_of_levels,
+                                                           len(inputs.steps)))
         electron_loss_collisions_array_new = numpy.zeros((2, inputs.number_of_levels, len(inputs.steps)))
         for i in range(inputs.number_of_levels):
             for j in range(inputs.number_of_levels):
@@ -28,8 +30,8 @@ class Rates:
                         x = temperature_array
                         y = electron_neutral_collisions_array[i, j, :], proton_neutral_collisions_array[i, j, :]
                         f = interp1d(x, y)
-                        electron_neutral_collisions_array_new[i, j, k] = f(inputs.electron_temperature)[0]
-                        proton_neutral_collisions_array_new[i, j, k] = f(inputs.proton_temperature)[1]
+                        electron_neutral_collisions_array_new[i, j, k] = f(inputs.electron_temperature[k])[0]
+                        proton_neutral_collisions_array_new[i, j, k] = f(inputs.proton_temperature[k])[1]
                     else:
                         continue
         for i in range(inputs.number_of_levels):
@@ -37,36 +39,36 @@ class Rates:
                 x = temperature_array
                 y = electron_loss_collisions_array[0, i, :], electron_loss_collisions_array[1, i, :]
                 f = interp1d(x, y)
-                electron_loss_collisions_array_new[0, i, k] = f(inputs.electron_temperature)[0]
-                electron_loss_collisions_array_new[1, i, k] = f(inputs.proton_temperature)[1]
+                electron_loss_collisions_array_new[0, i, k] = f(inputs.electron_temperature[k])[0]
+                electron_loss_collisions_array_new[1, i, k] = f(inputs.proton_temperature[k])[1]
 
-        self.electron_neutral_collisions=electron_neutral_collisions_array_new
-        self.proton_neutral_collisions=proton_neutral_collisions_array_new
-        self.electron_loss_collisions=electron_loss_collisions_array_new
-        self.einstein_coeffs=einstein_coeffs_array
-        self.mass = self.get_mass(beam_species = inputs.beam_species)
-        self.velocity=math.sqrt(2 * inputs.beam_energy * 1.602176487e-16 / self.mass)
-
+        self.electron_neutral_collisions = electron_neutral_collisions_array_new
+        self.proton_neutral_collisions = proton_neutral_collisions_array_new
+        self.electron_loss_collisions = electron_loss_collisions_array_new
+        self.einstein_coeffs = einstein_coeffs_array
+        self.mass = self.get_mass(beam_species=inputs.beam_species)
+        self.velocity = math.sqrt(2 * inputs.beam_energy * 1.602176487e-16 / self.mass)
 
     def setup_rate_coeff_arrays(self):
         local_dir = os.getcwd()
         file_name = 'rate_coeffs_' + str(self.inputs.beam_energy) + '_' + self.inputs.beam_species + '.h5'
         filename = self.locate_h5_dir(local_dir, self.inputs.beam_species) + '\\' + file_name
         temperature_array = self.get_data_from_hdf5(filename, 'Temperature axis')
-        electron_neutral_collisions_array = self.get_data_from_hdf5(filename,
-                                                               'Collisional Coeffs/Electron Neutral Collisions') / 1e4  # changing of units
-        proton_neutral_collisions_array = self.get_data_from_hdf5(filename,
-                                                             'Collisional Coeffs/Proton Neutral Collisions') / 1e4
-        impurity_neutral_collisions_array = self.get_data_from_hdf5(filename,
-                                                               'Collisional Coeffs/Impurity Neutral Collisions') / 1e4
-        electron_loss_collisions_array = self.get_data_from_hdf5(filename,
-                                                            'Collisional Coeffs/Electron Loss Collisions') / 1e4
+        # \1e4 - changing of units
+        electron_neutral_collisions_array =\
+            self.get_data_from_hdf5(filename, 'Collisional Coeffs/Electron Neutral Collisions') / 1e4
+        proton_neutral_collisions_array\
+            = self.get_data_from_hdf5(filename, 'Collisional Coeffs/Proton Neutral Collisions') / 1e4
+        impurity_neutral_collisions_array =\
+            self.get_data_from_hdf5(filename, 'Collisional Coeffs/Impurity Neutral Collisions') / 1e4
+        electron_loss_collisions_array =\
+            self.get_data_from_hdf5(filename, 'Collisional Coeffs/Electron Loss Collisions') / 1e4
         einstein_coeffs_array = self.get_data_from_hdf5(filename, 'Einstein Coeffs')
         impurity_collisions_array = self.get_data_from_hdf5(filename,
-                                                       'Impurity Collisions') / 1e4
-        rate_coeff_arrays=[temperature_array,electron_neutral_collisions_array,proton_neutral_collisions_array,\
-               impurity_neutral_collisions_array, electron_loss_collisions_array,\
-               einstein_coeffs_array, impurity_collisions_array]
+                                                            'Impurity Collisions') / 1e4
+        rate_coeff_arrays = [temperature_array, electron_neutral_collisions_array, proton_neutral_collisions_array,
+                             impurity_neutral_collisions_array, electron_loss_collisions_array,
+                             einstein_coeffs_array, impurity_collisions_array]
         return rate_coeff_arrays
 
     def get_mass(self, local_dir=os.getcwd(), beam_species='D'):
@@ -87,7 +89,6 @@ class Rates:
         else:
             return atom
 
-
     @staticmethod
     def get_data_from_hdf5(name, source):
         try:
@@ -99,4 +100,3 @@ class Rates:
             data = hdf5_id[source].value
             hdf5_id.close()
             return data
-    
