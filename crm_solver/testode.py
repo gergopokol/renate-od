0922504f8d56104b6b44127179f04ba58320c6e1
gergopokol <gergopokol@gmail.com>
2017-09-08 00:48:31 +0200

    #5 crm_solver/testode.py completed with test_size_of_set_up_equation_changing, test_size_of_set_up_equation_constant corrected.

--- crm_solver/testode.py
+++ crm_solver/testode.py
@@ -4,31 +4,42 @@ import numpy
 
 
 class TestOde(TestCase):
+    step_interval = 0.1
+    step_number = 100
+    steps = numpy.linspace(0, step_interval, step_number)
+
     test_initial_condition = numpy.array([1., 2.])
-    test_coefficient_matrix = numpy.array([[-0.2, 0.],
-                                           [0., -1.]])
+    test_coefficient_matrix_constant = numpy.array([[-0.2, 0.],
+                                                    [0., -1.]])
+    test_coefficient_matrix_changing = numpy.tensordot(test_coefficient_matrix_constant, steps, axes=0)
 
     test_initial_condition_1d = [numpy.array([0.]), ]
     test_coefficient_matrix_1d = [numpy.array([[2.]]), ]
     test_initial_condition_1d.append(numpy.array([1.]))
     test_coefficient_matrix_1d.append(numpy.array([[2.]]))
 
-    step_interval = 0.1
-    step_number = 100
-    steps = numpy.linspace(0, step_interval, step_number)
-
-    def test_set_up_equation(self):
-        ode = Ode(coefficient_matrix=self.test_coefficient_matrix,
+    def test_size_of_set_up_equation_constant(self):
+        ode = Ode(coefficient_matrix=self.test_coefficient_matrix_constant,
                   initial_condition=self.test_initial_condition,
                   steps=self.steps)
         s = ode.set_up_equation(variable_vector=self.test_initial_condition,
-                                actual_position=0,
-                                coefficient_matrix=self.test_coefficient_matrix,
-                                steps=[])
+                                actual_position=(self.steps[0]+self.steps[1])/2.,
+                                coefficient_matrix=self.test_coefficient_matrix_constant,
+                                steps=self.steps)
+        self.assertEqual(2, s.size)
+
+    def test_size_of_set_up_equation_changing(self):
+        ode = Ode(coefficient_matrix=self.test_coefficient_matrix_changing,
+                  initial_condition=self.test_initial_condition,
+                  steps=self.steps)
+        s = ode.set_up_equation(variable_vector=self.test_initial_condition,
+                                actual_position=(self.steps[0]+self.steps[1])/2.,
+                                coefficient_matrix=self.test_coefficient_matrix_changing,
+                                steps=self.steps)
         self.assertEqual(2, s.size)
 
     def test_size_of_solution(self):
-        ode = Ode(coefficient_matrix=self.test_coefficient_matrix,
+        ode = Ode(coefficient_matrix=self.test_coefficient_matrix_constant,
                   initial_condition=self.test_initial_condition,
                   steps=self.steps)
         s = ode.calculate_solution()
@@ -43,29 +54,29 @@ class TestOde(TestCase):
                     self.assertEqual(solution[index], self.formula_1d(init, coeff, variable))
 
     def test_diagonal_numerical(self):
-        ode = Ode(coefficient_matrix=self.test_coefficient_matrix,
+        ode = Ode(coefficient_matrix=self.test_coefficient_matrix_constant,
                   initial_condition=self.test_initial_condition,
                   steps=self.steps)
         solution = ode.calculate_solution()
         for i in range(self.step_number):
             for j in range(self.test_initial_condition.size):
                 self.assertAlmostEqual(solution[i, j], self.formula_1d(self.test_initial_condition[j],
-                                                                       self.test_coefficient_matrix[j, j],
+                                                                       self.test_coefficient_matrix_constant[j, j],
                                                                        self.steps[i]), 6)
 
     def test_diagonal_analytical(self):
-        ode = Ode(coefficient_matrix=self.test_coefficient_matrix,
+        ode = Ode(coefficient_matrix=self.test_coefficient_matrix_constant,
                   initial_condition=self.test_initial_condition,
                   steps=self.steps)
         solution = ode.analytical_solution()
         for i in range(self.step_number):
             for j in range(self.test_initial_condition.size):
                 self.assertAlmostEqual(solution[i, j], self.formula_1d(self.test_initial_condition[j],
-                                                                       self.test_coefficient_matrix[j, j],
+                                                                       self.test_coefficient_matrix_constant[j, j],
                                                                        self.steps[i]), 6)
 
     def test_numerical_to_analytical(self):
-        ode = Ode(coefficient_matrix=self.test_coefficient_matrix,
+        ode = Ode(coefficient_matrix=self.test_coefficient_matrix_constant,
                   initial_condition=self.test_initial_condition,
                   steps=self.steps)
         numerical = ode.calculate_solution()
