5a981b2cf4908d505a9c27eb892196ad3a536918
gergopokol <gergopokol@gmail.com>
2018-02-24 00:06:19 +0100

    #13 getdata.py ssh works, http does not, read_data() to be done

--- utility/getdata.py
+++ utility/getdata.py
@@ -9,32 +9,39 @@ class GetData:
         # Set constants that can later be read from setup files
         self.dummy_directory = "dummy"
         self.common_local_data_directory = "/data"
-        self.user_local_data_directory = "../data"
-        self.server_private_address = "data@deep.reak.bme.hu:private_html/renate-od"
-        self.server_public_address = "deep.reak.bme.hu/~data/renate-od"
+        self.user_local_data_directory = "data"
+        self.server_private_address = "data@deep.reak.bme.hu:~/private_html/renate-od"
+        self.private_key = "data/deep-data.ppk"
+        self.server_public_address = "http://deep.reak.bme.hu/~data/renate-od"
         self.contact_address = "pokol@reak.bme.hu"
 
         # Assemble paths
         self.data_path_name = data_path_name
-        self.common_local_data_path = self.common_local_data_directory + os.sep + data_path_name
-        self.user_local_data_path = self.user_local_data_directory + os.sep + data_path_name
-        self.user_local_dummy_path = self.user_local_data_directory + os.sep + self.dummy_directory
+        self.common_local_data_path = self.common_local_data_directory + '/' + data_path_name
+        self.user_local_data_path = self.user_local_data_directory + '/' + data_path_name
+        self.user_local_dummy_path = self.user_local_data_directory + '/' + self.dummy_directory + '/' + data_path_name
 
-        self.access_path = ''
+        self.read_data()
+
+    def read_data(self):
+        if self.get_data():
+            pass # Read to desired format (pandas.DataFrame?)
 
     def get_data(self):
         if self.check_common_local_data_path():
-            return
+            return True
         if self.check_user_local_data_path():
-            return
+            return True
         if self.get_private_data():
-            return
+            return True
         if self.check_user_local_dummy_path():
-            return
+            return True
         if self.get_public_data():
-            return
+            return True
         else:
+            print('Error: No data source available!')
             self.contact_with_us()
+            return False
 
     def contact_with_us(self):
         print('\nFor further info and data please contact us: \n\tmailto:' + self.contact_address)
@@ -57,15 +64,20 @@ class GetData:
 
     def get_private_data(self):
         server_private_path = self.server_private_address + "/" + self.data_path_name
-        if os.name == 'posix':
-            scp_answer = os.system('scp "%s" "%s"' % (server_private_path, self.user_local_data_path))
-        else:
-            scp_answer = os.system('winscp.exe "%s" "%s"' % (server_private_path, self.user_local_data_path))
-        if scp_answer == 1:
+        print('Attempting to download from server: ' + server_private_path)
+        try:
+            if os.name == 'posix':
+                scp_answer = os.system('scp -i "%s" "%s" "%s"' % (self.private_key, server_private_path, self.user_local_data_path))
+            else:
+                scp_answer = os.system('pscp -scp -i "%s" "%s" "%s"' % (self.private_key, server_private_path, self.user_local_data_path))
+        except:
+            scp_answer = 1
+        if scp_answer == 0:
             self.access_path = self.user_local_data_path
             print('Data has been downloaded from the server to the user local directory (' + self.access_path + ')')
             return True
         else:
+            print('Warning: Could not read data from server: ' + server_private_path + '!')
             return False
 
     def check_user_local_dummy_path(self):
@@ -78,14 +90,12 @@ class GetData:
 
     def get_public_data(self):
         server_public_path = self.server_public_address + "/" + self.data_path_name
+        print('Attempting to download from server: ' + server_public_path)
         try:
             urllib.request.urlretrieve(server_public_path, self.user_local_dummy_path)
             self.access_path = self.user_local_dummy_path
             print('Warning: Dummy data has been downloaded to the user local directory (' + self.access_path + ')!')
             return True
         except:
-            print('ERROR: Download was not successful!')
+            print('Warning: Could not read data from ' + server_public_path + '!')
             return False
-
-
-GetData("data.txt")
