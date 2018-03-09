2b3033e1f13dd46092bfbddf48793d8a3513ba02
gergopokol <gergopokol@gmail.com>
2018-03-09 20:36:19 +0100

    #5 References to Input removed from CoefficientMatrix and Rates.

--- crm_solver/coefficientmatrix.py
+++ crm_solver/coefficientmatrix.py
@@ -1,35 +1,37 @@
 import numpy
-from crm_solver import inputs
 from crm_solver.rates import Rates
 
 
 class CoefficientMatrix:
-    def __init__(self):
-        self.inputs = inputs.Constant_Plasma_Inputs()
-        self.rates = Rates()
+    def __init__(self, beamlet_param):
+        self.beamlet_param = beamlet_param
+        self.rates = Rates(beamlet_param)
         self.matrix = self.assemble()
 
     def assemble(self):
         coefficient_matrix = numpy.zeros(
-            (self.inputs.number_of_levels, self.inputs.number_of_levels, len(self.inputs.steps)))
-        for from_level in range(self.inputs.number_of_levels):
-            for to_level in range(self.inputs.number_of_levels):
-                for step in range(len(self.inputs.steps)):
+            (self.beamlet_param.number_of_levels, self.beamlet_param.number_of_levels, len(self.beamlet_param.steps)))
+        for from_level in range(self.beamlet_param.number_of_levels):
+            for to_level in range(self.beamlet_param.number_of_levels):
+                for step in range(len(self.beamlet_param.steps)):
                     if to_level == from_level:
-                        electron_terms = sum(self.rates.electron_neutral_collisions[from_level, :from_level, step]) + sum(
-                            self.rates.electron_neutral_collisions[from_level, from_level + 1:self.inputs.number_of_levels, step]) + \
-                            self.rates.electron_loss_collisions[0, from_level, step]
-                        ion_terms = sum(self.rates.proton_neutral_collisions[from_level, :from_level, step]) + sum(
-                            self.rates.proton_neutral_collisions[from_level, from_level + 1:self.inputs.number_of_levels, step]) + \
-                            self.rates.electron_loss_collisions[1, from_level, step]
+                        electron_terms = sum(self.rates.electron_neutral_collisions[from_level, :from_level, step]) + \
+                                         sum(self.rates.electron_neutral_collisions[from_level, from_level + \
+                                            1:self.beamlet_param.number_of_levels, step]) + \
+                                            self.rates.electron_loss_collisions[0, from_level, step]
+                        ion_terms = sum(self.rates.proton_neutral_collisions[from_level, :from_level, step]) + \
+                                    sum(self.rates.proton_neutral_collisions[from_level, from_level + \
+                                        1:self.beamlet_param.number_of_levels, step]) + \
+                                        self.rates.electron_loss_collisions[1, from_level, step]
                         photon_terms = sum(self.rates.einstein_coeffs[:, from_level]) / self.rates.velocity
-                        coefficient_matrix[from_level, from_level, step] = -self.inputs.density[step] * electron_terms \
-                                                      - self.inputs.density[step] * ion_terms \
-                                                      - photon_terms
+                        coefficient_matrix[from_level, from_level, step] = \
+                            -self.beamlet_param.profiles.density[step] * electron_terms - \
+                            self.beamlet_param.profiles.density[step] * ion_terms - \
+                            photon_terms
                     else:
-                        coefficient_matrix[from_level, to_level, step] = self.inputs.density[step] \
+                        coefficient_matrix[from_level, to_level, step] = self.beamlet_param.profiles.density[step] \
                                                       * self.rates.electron_neutral_collisions[to_level, from_level, step] \
-                                                      + self.inputs.density[step] \
+                                                      + self.beamlet_param.profiles.density[step] \
                                                       * self.rates.proton_neutral_collisions[to_level, from_level, step] \
                                                       + self.rates.einstein_coeffs[from_level, to_level] / self.rates.velocity
         return coefficient_matrix
--- crm_solver/rates.py
+++ crm_solver/rates.py
@@ -1,16 +1,14 @@
-import h5py
-import os
 import numpy
 import math
+import utility
 from scipy.interpolate import interp1d
-from crm_solver import inputs
-from utility import get_data_from_hdf5
 
 
 class Rates:
     # Get rate coefficients from hdf5 files:
-    def __init__(self):
-        self.inputs = inputs.Constant_Plasma_Inputs()
+    def __init__(self,beamlet_param,rate_type='default'):
+        self.beamlet_param = beamlet_param
+        self.rate_type = rate_type
         rate_coefficients = self.setup_rate_coeff_arrays()
         temperature_array = rate_coefficients[0]
         electron_neutral_collisions_array = rate_coefficients[1]
@@ -19,77 +17,88 @@ class Rates:
         einstein_coeffs_array = rate_coefficients[5]
 
         # Interpolate rate coeffs to new grid:
-        electron_neutral_collisions_array_new = numpy.zeros((self.inputs.number_of_levels, self.inputs.number_of_levels,
-                                                             len(self.inputs.steps)))
-        proton_neutral_collisions_array_new = numpy.zeros((self.inputs.number_of_levels, self.inputs.number_of_levels,
-                                                           len(self.inputs.steps)))
-        electron_loss_collisions_array_new = numpy.zeros((2, self.inputs.number_of_levels, len(self.inputs.steps)))
-        for from_level in range(self.inputs.number_of_levels):
-            for to_level in range(self.inputs.number_of_levels):
-                for step in range(len(self.inputs.steps)):
+        electron_neutral_collisions_array_new = numpy.zeros((self.beamlet_param.number_of_levels,
+                                                             self.beamlet_param.number_of_levels,
+                                                             len(self.beamlet_param.steps)))
+        proton_neutral_collisions_array_new = numpy.zeros((self.beamlet_param.number_of_levels,
+                                                           self.beamlet_param.number_of_levels,
+                                                           len(self.beamlet_param.steps)))
+        electron_loss_collisions_array_new = numpy.zeros((2, self.beamlet_param.number_of_levels,
+                                                          len(self.beamlet_param.steps)))
+        for from_level in range(self.beamlet_param.number_of_levels):
+            for to_level in range(self.beamlet_param.number_of_levels):
+                for step in range(len(self.beamlet_param.steps)):
                     if to_level != from_level:
                         x = temperature_array
                         y = electron_neutral_collisions_array[from_level, to_level, :], \
                             proton_neutral_collisions_array[from_level, to_level, :]
                         f = interp1d(x, y)
                         electron_neutral_collisions_array_new[from_level, to_level, step] =\
-                            f(self.inputs.electron_temperature[step])[0]
+                            f(self.beamlet_param.profiles.electron_temperature[step])[0]
                         proton_neutral_collisions_array_new[from_level, to_level, step]\
-                            = f(self.inputs.ion_temperature[step])[1]
+                            = f(self.beamlet_param.profiles.ion_temperature[step])[1]
                     else:
                         continue
-        for from_level in range(self.inputs.number_of_levels):
-            for step in range(len(self.inputs.steps)):
+        for from_level in range(self.beamlet_param.number_of_levels):
+            for step in range(len(self.beamlet_param.steps)):
                 x = temperature_array
                 y = electron_loss_collisions_array[0, from_level, :], electron_loss_collisions_array[1, from_level, :]
                 f = interp1d(x, y)
-                electron_loss_collisions_array_new[0, from_level, step] = f(self.inputs.electron_temperature[step])[0]
-                electron_loss_collisions_array_new[1, from_level, step] = f(self.inputs.ion_temperature[step])[1]
+                electron_loss_collisions_array_new[0, from_level, step] = \
+                    f(self.beamlet_param.profiles.electron_temperature[step])[0]
+                electron_loss_collisions_array_new[1, from_level, step] = \
+                    f(self.beamlet_param.profiles.ion_temperature[step])[1]
 
         self.electron_neutral_collisions = electron_neutral_collisions_array_new
-
         self.proton_neutral_collisions = proton_neutral_collisions_array_new
         self.electron_loss_collisions = electron_loss_collisions_array_new
         self.einstein_coeffs = einstein_coeffs_array
-        self.mass = self.get_mass(beam_species=self.inputs.beam_species)
-        self.velocity = math.sqrt(2 * self.inputs.beam_energy * 1.602176487e-16 / self.mass)
+        self.mass = self.get_mass()
+        self.velocity = math.sqrt(2 * self.beamlet_param.beam_energy * 1.602176487e-16 / self.mass)
 
     def setup_rate_coeff_arrays(self):
-        local_dir = os.getcwd()
-        file_name = 'rate_coeffs_' + str(self.inputs.beam_energy) + '_' + self.inputs.beam_species + '.h5'
-        filename = self.locate_h5_dir(local_dir, self.inputs.beam_species) + '/' + file_name
-        temperature_array = get_data_from_hdf5.get_data_from_hdf5(filename, 'Temperature axis')
-        # \1e4 - changing of units
-        electron_neutral_collisions_array =\
-            get_data_from_hdf5.get_data_from_hdf5(filename, 'Collisional Coeffs/Electron Neutral Collisions') / 1e4
-        proton_neutral_collisions_array\
-            = get_data_from_hdf5.get_data_from_hdf5(filename, 'Collisional Coeffs/Proton Neutral Collisions') / 1e4
-        impurity_neutral_collisions_array =\
-            get_data_from_hdf5.get_data_from_hdf5(filename, 'Collisional Coeffs/Impurity Neutral Collisions') / 1e4
-        electron_loss_collisions_array =\
-            get_data_from_hdf5.get_data_from_hdf5(filename, 'Collisional Coeffs/Electron Loss Collisions') / 1e4
-        einstein_coeffs_array = get_data_from_hdf5.get_data_from_hdf5(filename, 'Einstein Coeffs')
-        impurity_collisions_array = get_data_from_hdf5.get_data_from_hdf5(filename,
-                                                           'Impurity Collisions') / 1e4
+        file_name = 'rate_coeffs_' + str(self.beamlet_param.beam_energy) + '_' + \
+                    self.beamlet_param.beam_species + '.h5'
+        data_path_name = self.locate_h5_dir() + file_name
+        temperature_array = utility.getdata.GetData(data_path_name, 'Temperature axis').data
+        electron_neutral_collisions_array = \
+            utility.getdata.GetData(data_path_name, 'Collisional Coeffs/Electron Neutral Collisions').data
+        proton_neutral_collisions_array = \
+            utility.getdata.GetData(data_path_name, 'Collisional Coeffs/Proton Neutral Collisions').data
+        impurity_neutral_collisions_array = \
+            utility.getdata.GetData(data_path_name, 'Collisional Coeffs/Impurity Neutral Collisions').data
+        electron_loss_collisions_array = \
+            utility.getdata.GetData(data_path_name, 'Collisional Coeffs/Electron Loss Collisions').data
+        einstein_coeffs_array = utility.getdata.GetData(data_path_name, 'Einstein Coeffs').data
+        impurity_collisions_array = utility.getdata.GetData(data_path_name,
+                                                           'Impurity Collisions').data
+        # This is to be removed when input file is in SI
+        electron_neutral_collisions_array = self.convert_from_cm2_to_m2(electron_neutral_collisions_array)
+        proton_neutral_collisions_array = self.convert_from_cm2_to_m2(proton_neutral_collisions_array)
+        impurity_neutral_collisions_array = self.convert_from_cm2_to_m2(impurity_neutral_collisions_array)
+        electron_loss_collisions_array = self.convert_from_cm2_to_m2(electron_loss_collisions_array)
+        impurity_collisions_array = self.convert_from_cm2_to_m2(impurity_collisions_array)
+        # To-be-removed end
+
         rate_coeff_arrays = [temperature_array, electron_neutral_collisions_array, proton_neutral_collisions_array,
                              impurity_neutral_collisions_array, electron_loss_collisions_array,
                              einstein_coeffs_array, impurity_collisions_array]
         return rate_coeff_arrays
 
-    def get_mass(self, local_dir=os.getcwd(), beam_species='Li'):
-        mass_kg = numpy.loadtxt(self.locate_h5_dir(local_dir, beam_species) + '/Mass/' + beam_species + '_m.txt')
-        return mass_kg
+    def get_mass(self):
+        data_path_name = 'atomic_data/' + self.beamlet_param.beam_species + '/supplementary_data/default/' + \
+                         self.beam_species + '_m.txt'
+        mass_str = utility.getdata.GetData(data_path_name = data_path_name).data
+        try:
+            mass = float(mass_str)
+        except ValueError:
+            print('Unexpected data in file: ' + data_path_name + '(Expecting single float!)')
+            raise ValueError
+        return mass
 
-    def locate_h5_dir(self, cwd, atom):
-        rod_loc = (str.find(cwd, 'trunk'))
-        atom_folder = self.choose_atom_folder(atom)
-        return cwd[0:rod_loc] + 'trunk/data/' + atom_folder
+    def locate_h5_dir(self):
+        return 'atomic_data/' + self.beamlet_param.beam_species + '/rates/' + self.rate_type + '/'
 
     @staticmethod
-    def choose_atom_folder(atom):
-        if atom == 'H':
-            return 'H bundled-n'
-        elif atom == 'D':
-            return 'D bundled-n'
-        else:
-            return atom
+    def convert_from_cm2_to_m2(cross_section):
+        return cross_section / 1.e4
