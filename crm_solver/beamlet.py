d9271e18d8f84feac8923d6e54cf35f35c19a774
gergopokol <gergopokol@gmail.com>
2018-03-21 19:41:28 +0100

    #5 Now gets profile data from profiles key of the HDF5.

--- crm_solver/beamlet.py
+++ crm_solver/beamlet.py
@@ -24,7 +24,7 @@ class Beamlet:
 
     def read_beamlet_profiles(self):
         hdf5_path = self.param.getroot().find('body').find('beamlet_profiles').text
-        self.profiles = utility.getdata.GetData(data_path_name=hdf5_path).data
+        self.profiles = utility.getdata.GetData(data_path_name=hdf5_path, data_key='profiles').data
         assert isinstance(self.profiles, pandas.DataFrame)
         print('Beamlet.profiles read from file: ' + hdf5_path)
 
