c0b76f74359c7665503a4a385bf460e1b3501f41
asztalosors <asztalos@reak.bme.hu>
2018-03-22 15:08:17 +0100

    #10 Corrections to int -> str transformation in error handling

--- imas_utility/idscoreprof.py
+++ imas_utility/idscoreprof.py
@@ -6,12 +6,12 @@ import os
 class CoreprofIds(ImasObject):
     def __init__(self, shot, run, user=None, machine=None):
         super().__init__(shot, run, user, machine)
-        self.load_coreprof_ids()
+        self.load_core_profiles_ids()
 
-    def load_coreprof_ids(self):
+    def load_core_profiles_ids(self):
         try:
-            self.coreprof = self.imas_pointer.get('core_profiles')
+            self.core_profiles = self.imas_pointer.get('core_profiles')
         except:
             print('The core_profiles IDS is absent from data file. Muhaha.')
-            print('Please get more information about shot ' + self.shot + ' at run ' + self.run)
+            print('Please get more information about shot ' + str(self.shot) + ' at run ' + str(self.run))
             exit()
