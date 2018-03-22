981f8dcbd1ef96e7fa23d6dd229eec25909cec68
asztalosors <asztalos@reak.bme.hu>
2018-03-22 15:19:18 +0100

    #10 Correction to equilibrium IDS getter. Correcting inheritance issues as well.

--- imas_utility/idsequilibrium.py
+++ imas_utility/idsequilibrium.py
@@ -2,15 +2,15 @@ from imas_utility.idsinstance import ImasObject
 import numpy as np
 
 
-class EquilibriumIds:
+class EquilibriumIds(ImasObject):
     def __init__(self, shot, run, user=None, machine=None):
-        self.ids = ImasObject(shot, run, user=user, machine=machine)
+        super().__init__(shot, run, user, machine)
         self.load_equilibrium_ids()
 
     def load_equilibrium_ids(self):
         try:
-            self.equilibrium = self.ids.imas_pointer.get('equilibrium')
+            self.equilibrium = self.imas_pointer.get('equilibrium')
         except:
-            print, 'The equilibrium IDS is absent from data file. Muhaha.'
-            print, 'Please get more information about shot ' + self.shot + ' at run ' + self.run
+            print('The equilibrium IDS is absent from data file. Muhaha.')
+            print('Please get more information about shot ' + str(self.shot) + ' at run ' + str(self.run))
             exit()
