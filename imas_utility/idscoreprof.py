1b9b79622aa45ff127d94d07ec05e78d305cbdbb
asztalosors <asztalos@reak.bme.hu>
2018-03-22 14:08:45 +0100

    #10 Making corrections to inheritance and init of subclasses. Needs to be tested on iter cluster.

--- imas_utility/idscoreprof.py
+++ imas_utility/idscoreprof.py
@@ -3,14 +3,14 @@ import numpy as np
 import os
 
 
-class CoreprofIds:
+class CoreprofIds(ImasObject):
     def __init__(self, shot, run, user=None, machine=None):
-        self.ids = ImasObject(shot, run, user=user, machine=machine)
+        super().__init__(self, shot, run, user=user, machine=machine)
         self.load_coreprof_ids()
 
     def load_coreprof_ids(self):
         try:
-            self.coreprof = self.ids.imas_pointer.get('coreprof')
+            self.coreprof = self.imas_pointer.get('coreprof')
         except:
             print, 'The coreprof IDS is absent from data file. Muhaha.'
             print, 'Please get more information about shot ' + self.shot + ' at run ' + self.run
