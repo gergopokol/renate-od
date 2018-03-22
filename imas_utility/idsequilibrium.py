240f459d7ca8fcfbe3adca48d1ba828858414395
asztalosors <asztalos@reak.bme.hu>
2018-03-22 11:48:21 +0100

    #10 Adding the routine that initializes the equilibrium IDS as well. Some minor PEP8 corrections as well to both routines

--- imas_utility/idsequilibrium.py
+++ imas_utility/idsequilibrium.py
@@ -1,17 +1,16 @@
 from imas_utility.idsinstance import ImasObject
 import numpy as np
-import os
 
 
-class CoreprofIds:
+class EquilibriumIds:
     def __init__(self, shot, run, user=None, machine=None):
         self.ids = ImasObject(shot, run, user=user, machine=machine)
-        self.load_coreprof_ids()
+        self.load_equilibrium_ids()
 
-    def load_coreprof_ids(self):
+    def load_equilibrium_ids(self):
         try:
-            self.coreprof = self.ids.imas_pointer.get('coreprof')
+            self.equilibrium = self.ids.imas_pointer.get('equilibrium')
         except:
-            print, 'The coreprof IDS is absent from data file. Muhaha.'
+            print, 'The equilibrium IDS is absent from data file. Muhaha.'
             print, 'Please get more information about shot ' + self.shot + ' at run ' + self.run
             exit()
