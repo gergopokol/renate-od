4acaab805adae491b3b0dedb10470f8479b80da7
asztalosors <asztalos@reak.bme.hu>
2018-03-22 09:30:04 +0100

    #10 Adding a preliminary version of the IDS init routine. This creatres and object that checks and loads the IDS if it exists

---
+++ imas-utility/idsinstance.py
@@ -0,0 +1,40 @@
+import pyual
+import os
+
+
+class ImasObject:
+    def __init__(self, shot, run, user=None, machine=None):
+        assert isinstance(shot, int)
+        self.shot = shot
+
+        assert isinstance(run, int)
+        self.run = run
+
+        if user is None:
+            self.user = os.getenv('USER')
+            self.local_data_base = False
+        else:
+            assert isinstance(user, str)
+            self.user = user
+            self.local_data_base = True
+
+        if machine is None:
+            self.machine = 'iter'
+        else:
+            assert isinstance(machine, str)
+            self.machine = machine
+
+        self.load_ids()
+
+    def load_ids(self):
+        try:
+            if self.local_data_base is True:
+                self.ids = pyual.Client(self.shot, self.run, user=self.user, machine=self.machine)
+            else:
+                self.ids = pyual.Client(self.shot, self.run)
+        except:
+            print, 'Shot ' + str(self.shot) + ', run ' + str(self.run) + ' could not be opened'
+            print, 'IDS load ----> Aborted. Check for the existence of IDS in one of the following locations:'
+            print, '1. /work/imas/shared/'+self.machine+'db/3/0/....'
+            print, '2. /home/ITER/user/public/imasdb/....'
+            exit()
