bb8731dfedcb86ba74dc698d472570d5babdbda3
SzondyB <szondy.borbala@gmail.com>
2018-04-05 13:01:59 +0200

    #5 Cefficient matrix indices changed, benchmark was succesful

--- utility/test_solution.py
+++ utility/test_solution.py
@@ -2,6 +2,7 @@ import os
 import h5py
 import sys
 import matplotlib.pyplot
+import matplotlib.cm
 import pandas
 from utility import getdata
 from crm_solver import beamlet
@@ -10,15 +11,15 @@ from lxml import etree
 hdf5_id = h5py.File('../data/beamlet/test_profiles.h5', 'r')
 print(hdf5_id['benchmark']['renate_data_values'].value)
 
-matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[0,:],'--')
-matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[1,:],'--')
-matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[2,:],'--')
-matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[3,:],'--')
-matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[4,:],'--')
-matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[5,:],'--')
-matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[6,:],'--')
-matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[7,:],'--')
-matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[8,:],'--')
+matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[0,:],'o',label='RENATE - 0')
+matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[1,:],'o',label='RENATE - 1')
+matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[2,:],'o',label='RENATE - 2')
+matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[3,:],'o',label='RENATE - 3')
+matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[4,:],'o',label='RENATE - 4')
+matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[5,:],'o',label='RENATE - 5')
+matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[6,:],'o',label='RENATE - 6')
+matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[7,:],'o',label='RENATE - 7')
+matplotlib.pyplot.plot(hdf5_id['profiles']['block0_values'].value[:,2],hdf5_id['benchmark']['renate_data_values'].value[8,:],'o',label='RENATE - 8')
 matplotlib.pyplot.yscale('log')
 
 os.chdir('../')
