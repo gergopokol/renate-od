7b01fccd91c8094fa884c8e4623578385d5ab006
asztalosors <asztalos@reak.bme.hu>
2018-04-10 19:12:04 +0200

    #10 Adding the equilibrium ids file to the init file

--- imas_utility/__init__.py
+++ imas_utility/__init__.py
@@ -2,3 +2,5 @@ from imas_utility import idsinstance
 from imas_utility import idscoreprof
 from imas_utility import idsnbi
 from imas_utility import idsspectrometervisible
+from imas_utility import idsequilibrium
+
