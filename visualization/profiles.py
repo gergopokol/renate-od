a6fe5ca6b88fdb12c054996174da9b1fb48a8a8e
SzondyB <szondy.borbala@gmail.com>
2018-04-11 16:10:39 +0200

    #7 Filename changed  - file names should not start with number!!! Visualization of 1d beam profiles from existing hdf5 file, successfully tested

--- visualization/profiles.py
+++ visualization/profiles.py
@@ -1,14 +1,10 @@
 import matplotlib.pyplot
+import pandas
 
-from imas_wrapper.wrapper_1d import BeamletFromIds
 
-
-class Visualize:
-    def __init__(self, profiles='None'):
-        if profiles == 'None':
-            bfi = BeamletFromIds()
-            bfi.compute_beamevolution()
-            self.profiles = bfi.profiles
+class Profiles:
+    def __init__(self, access_path):
+        self.profiles = pandas.read_hdf(access_path)
         self.plot_populations()
 
     def plot_populations(self):
@@ -17,7 +13,7 @@ class Visualize:
             matplotlib.pyplot.plot(self.profiles['beamlet_grid'], self.profiles[label], label=label)
             matplotlib.pyplot.yscale('log', nonposy='clip')
             matplotlib.pyplot.ylim((1e-5, 1))
-        matplotlib.pyplot.legend(loc='best', bbox_to_anchor=(1, 0.5), ncol=1)
+        matplotlib.pyplot.legend(loc='best', bbox_to_anchor=(0, 0.5), ncol=1)
         matplotlib.pyplot.xlabel('x')
         matplotlib.pyplot.grid()
         matplotlib.pyplot.show()
