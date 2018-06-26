cab98dc903197ae57ae869928d363e56c57ebd74
gergopokol <gergopokol@gmail.com>
2018-06-26 19:22:12 +0200

    #15 convert.py routines commenting and clarifying.

--- utility/convert.py
+++ utility/convert.py
@@ -5,13 +5,18 @@ import utility
 
 
 def convert_beamlet_profiles(data_path_name='data/beamlet/test_profiles.h5'):
-    h5file = h5py.File(data_path_name,'r')
+    """
+    Convert HDF profiles file saved by RENATE to HDF file of the format of RENATE-OD.
+    :param data_path_name: Old file is copied to .old, and then overwritten.
+    :return: 
+    """
+    h5file = h5py.File(data_path_name, 'r')
     pandas_profiles = pandas.DataFrame()
     columns = list(h5file.keys())
     for column in columns:
         pandas_profiles[column] = h5file[column].value
     h5file.close()
-    os.rename(data_path_name,data_path_name + '.old')
+    os.rename(data_path_name, data_path_name + '.old')
     pandas_profiles.to_hdf(data_path_name)
 
 
@@ -23,7 +28,7 @@ def convert_from_cm_to_m(length):
         return length / 1.e2
 
 
-def convert_from_10_9_to_1(density):
+def convert_from_10_19_to_1(density):
     return density * 1.e19
 
 
@@ -31,7 +36,7 @@ def convert_beamlet_profiles_to_si(data_path_name='beamlet/test_profiles.h5'):
     pandas_profiles = utility.getdata.GetData(data_path_name=data_path_name).data
     assert isinstance(pandas_profiles, pandas.DataFrame)
     full_data_path_name = 'data/' + data_path_name
-    pandas_profiles['beamlet_density'] = convert_from_10_9_to_1(pandas_profiles['beamlet_density'])
+    pandas_profiles['beamlet_density'] = convert_from_10_19_to_1(pandas_profiles['beamlet_density'])
     # pandas_profiles['beamlet_grid'] = convert_from_cm_to_m(pandas_profiles['beamlet_grid'])
     os.rename(full_data_path_name,full_data_path_name + '.non-si')
     pandas_profiles.to_hdf(full_data_path_name,'profiles')
