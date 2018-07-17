733f5af59e8b665c63d8dddbe85afe73cc8b8c84
SzondyB <szondy.borbala@gmail.com>
2018-07-17 14:31:38 +0200

    #7 profiles.py refactored

--- visualization/profiles.py
+++ visualization/profiles.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot
 import pandas
-import h5py
 
 
 class Profiles:
@@ -11,38 +10,32 @@ class Profiles:
 
     def plot_populations(self):
         axis = matplotlib.pyplot.subplot()
-        axis = self.setup_population_axis(axis)
+        self.setup_population_axis(axis)
         matplotlib.pyplot.show()
 
     def plot_all_profiles(self):
         fig1 = matplotlib.pyplot.figure()
-        grid = matplotlib.pyplot.GridSpec(3,1)
-        ax1 = matplotlib.pyplot.subplot(grid[0,0])
+        grid = matplotlib.pyplot.GridSpec(3, 1)
+        ax1 = matplotlib.pyplot.subplot(grid[0, 0])
         ax1 = self.setup_density_axis(ax1)
         ax2 = ax1.twinx()
-        ax2 = self.setup_temperature_axis(ax2)
+        self.setup_temperature_axis(ax2)
         ax1.set_title('Plasma profiles')
-        ax3 = matplotlib.pyplot.subplot(grid[1:,0])
-        ax3 = self.setup_population_axis(ax3)
+        ax3 = matplotlib.pyplot.subplot(grid[1:, 0])
+        self.setup_population_axis(ax3)
         fig1.tight_layout()
         matplotlib.pyplot.show()
 
-    def benchmark_to_renate(self):
+    def benchmark(self, path='../data/beamlet/IMAS_beamlet_test_profiles_Li.h5'):
         fig1 = matplotlib.pyplot.figure()
         ax1 = matplotlib.pyplot.subplot()
-        ax1=self.setup_population_axis(ax1)
+        self.setup_population_axis(ax1)
+        self.setup_benchmark_axis(path, axis=ax1)
         ax1.legend(loc='best', ncol=2)
-        ax1.set_title('Beamlet profiles - benchmark of RENATE-OD to RENATE')
+        ax1.set_title('Beamlet profiles - benchmark')
         ax1.grid()
         fig1.tight_layout()
         matplotlib.pyplot.show()
-
-    def get_number_of_levels(self):
-        levels=self.profiles.filter(like='level', axis=1)
-        number_of_levels = len(levels.keys())
-        if number_of_levels ==0:
-            number_of_levels = 9
-        return number_of_levels
 
     def setup_density_axis(self, axis):
         axis.plot(self.profiles['beamlet_grid'], self.profiles['beamlet_density'], label='Density', color='b')
@@ -54,8 +47,9 @@ class Profiles:
 
     def setup_temperature_axis(self, axis):
         axis.plot(self.profiles['beamlet_grid'], self.profiles['beamlet_electron_temp'], color='r',
-                               label='Electron_temperature')
-        axis.plot(self.profiles['beamlet_grid'], self.profiles['beamlet_ion_temp'], '--', label='Ion_temperature', color='m')
+                  label='Electron_temperature')
+        axis.plot(self.profiles['beamlet_grid'], self.profiles['beamlet_ion_temp'], '--', label='Ion_temperature',
+                  color='m')
         axis.set_ylabel('Temperature [eV]')
         axis.yaxis.label.set_color('r')
         axis.legend(loc='lower right')
@@ -63,7 +57,7 @@ class Profiles:
         return axis
 
     def setup_population_axis(self, axis):
-        number_of_levels = self.get_number_of_levels()
+        number_of_levels = self.get_number_of_levels(self.profiles)
         for level in range(number_of_levels):
             label = 'level ' + str(level)
             axis.plot(self.profiles['beamlet_grid'], self.profiles[label], label=label)
@@ -76,8 +70,18 @@ class Profiles:
         axis.grid()
         return axis
 
-    def setup_benchmark_axis(self):
-        benchmark_profiles = h5py.File(self.access_path, 'r')
-        for i in range(9):
-            matplotlib.pyplot.plot(self.profiles['beamlet_grid'], benchmark_profiles['benchmark']['renate_data_values'].value[i, :],
-                      label='RENATE - level ' + str(i))
+    def setup_benchmark_axis(self, path, axis):
+        benchmark_profiles = pandas.read_hdf(path, self.key)
+        number_of_levels = self.get_number_of_levels(benchmark_profiles)
+        for level in range(number_of_levels):
+            label = 'level ' + str(level)
+            axis.plot(benchmark_profiles['beamlet_grid'], benchmark_profiles[label], '--', label=label+' bm.')
+        return axis
+
+    @staticmethod
+    def get_number_of_levels(profiles):
+        levels = profiles.filter(like='level', axis=1)
+        number_of_levels = len(levels.keys())
+        if number_of_levels == 0:
+            number_of_levels = 9
+        return number_of_levels
