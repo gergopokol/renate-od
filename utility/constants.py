4eee39e2ad715b187dcea3d71b1800e3b42ef27b
SzondyB <szondy.borbala@gmail.com>
2018-08-07 13:54:46 +0200

    #15 New utility file created: constants.py This is a temporary solution for storing constants, IMAS related applications and further possibilities to be discussed later. The aim is to avoid unnecessary reproduction of class instances. Issue to be discussed: do we want to create a Constants() class?

---
+++ utility/constants.py
@@ -0,0 +1,7 @@
+class Constants:
+    def __init__(self):
+        self.charge_electron = 1.602176487e-16   #[C]
+        self.mass = 1.1525*1e-26
+        self.speed_of_light = 299792458       #[m / s]
+        self.einstein = 3.72*1e7
+
