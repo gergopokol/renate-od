1168b6467aedc62d84fa6901dfeaeb94a828470c
SzondyB <szondy.borbala@gmail.com>
2018-08-08 11:50:16 +0200

    #8 	input_obj -> beamlet 	input_param -> beamlet.param  	input_profiles -> beamlet.profiles

--- observation/observation.py
+++ observation/observation.py
@@ -9,14 +9,11 @@ from pdb import set_trace as bp
 
 
 class Obs1d:
-    def __init__(self, input_obj, data_path, beam_current):
-        self.input_param = input_obj.param
-        self.input_profiles = input_obj.profiles
+    def __init__(self, beamlet, data_path, beam_current):
+        self.beamlet = beamlet
         self.constants = Constants()
-        self.velocity = convert.calculate_velocity_from_energy(self.input_param.getroot().find('body').find('beamlet_energy').text,
-                                                               getdata.get_mass(
-                                                                   self.input_param.getroot().find('body').find('beamlet_species').text))
-        self.photon_fraction = beam_current/self.constants.charge_electron/self.velocity
+        self.photon_fraction = beam_current/self.constants.charge_electron\
+                               / float(beamlet.param.getroot().find('body').find('velocity').text)
         self.obs_param = self.read_observation_param(data_path)
         self.obs_profile = self.read_observation_profile(data_path=self.obs_param.getroot().find('body').find('observation_profile_path').text)
 
@@ -25,12 +22,12 @@ class Obs1d:
         for detector_index in range(len(self.obs_profile)):
             loc_up = self.obs_profile[0][detector_index] + float(self.obs_param.getroot().find('body').find('detector_size').text)/ 2
             loc_down = self.obs_profile[0][detector_index] - float(self.obs_param.getroot().find('body').find('detector_size').text)/ 2
-            position_index_1 = numpy.where((self.input_profiles.beamlet_grid < loc_up))
-            position_index_2 = numpy.where((self.input_profiles.beamlet_grid > loc_down))
+            position_index_1 = numpy.where((self.beamlet.profiles.beamlet_grid < loc_up))
+            position_index_2 = numpy.where((self.beamlet.profiles.beamlet_grid > loc_down))
             low = list(position_index_1)
             up = list(position_index_2)
             for step_index in range(up[0][0], low[0][-1]):
-                light_profile[detector_index] = light_profile[detector_index] + self.input_profiles['level 2'][step_index] \
+                light_profile[detector_index] = light_profile[detector_index] + self.beamlet.profiles['level 2'][step_index] \
                                                                                 * self.photon_fraction
         return light_profile
 
