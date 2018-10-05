3cc190628d0f318e1910c8aa09220496f9ef8ff0
asztalosors <asztalos@reak.bme.hu>
2018-10-05 13:49:47 +0200

    #10 __init__ changed to reflect renames.

--- imas_utility/__init__.py
+++ imas_utility/__init__.py
@@ -1,5 +1,5 @@
 from imas_utility import idsinstance
-from imas_utility import idscoreprof
+from imas_utility import idsprofiles
 from imas_utility import idsnbi
 from imas_utility import idsspectrometervisible
 from imas_utility import idsequilibrium
