96901402c350473923d7cd35ae0aea973a6f0962
SzondyB <szondy.borbala@gmail.com>
2018-10-05 15:59:46 +0200

    #8 Several modifications in Obs1d: 	- photon emission is now set to zero if the detector is not completely inside the calculated region given by the beamlet grid 	- completion of photon_fraction equation by a missing multiplier (Einstein coeff.) 	- the Einstein coefficient used is the one from the observed level specified in the input xml file to level 0 	- etendue has to be taken into consideration, therefore renaming was necessary: light profile -> photon emission profile

--- observation/observation.py
+++ observation/observation.py
@@ -5,6 +5,7 @@ import math
 from utility.constants import Constants
 from utility import convert
 from utility import getdata
+import re
 from pdb import set_trace as bp
 
 
@@ -12,22 +13,28 @@ class Obs1d:
     def __init__(self, beamlet, data_path, beam_current):
         self.beamlet = beamlet
         self.constants = Constants()
-        self.photon_fraction = beam_current/self.constants.charge_electron\
-                               / float(beamlet.param.getroot().find('body').find('beamlet_velocity').text)
         self.obs_param = self.read_observation_param(data_path)
         self.obs_profile = self.read_observation_profile(data_path=self.obs_param.getroot().find('body').find('observation_profile_path').text)
+        self.observed_level = self.obs_param.getroot().find('body').find('observed_level').text
+        self.observed_level_index = int(re.findall('\\d+', self.observed_level)[0])
+        self.photon_fraction = beam_current * self.beamlet.coefficient_matrix.rates.einstein_coeffs[0, self.observed_level_index]\
+                               / self.constants.charge_electron / float(beamlet.param.getroot().find('body').find('beamlet_velocity').text)
+        self.photon_emission_profile = numpy.zeros(self.obs_profile.size)
 
-    def calculate_light_profile(self):
-        light_profile = numpy.zeros(self.obs_profile.size)
+    def calculate_photon_emission_profile(self):
         for detector_index in range(len(self.obs_profile)):
-            detector_steps = numpy.where(abs(self.beamlet.profiles.beamlet_grid-self.obs_profile[0][detector_index])
-                                         < float(self.obs_param.getroot().find('body').find('detector_size').text) / 2)
-            for step_index in detector_steps[0]:
-                light_profile[detector_index] = light_profile[detector_index] + \
-                                                self.beamlet.profiles[
-                                                    self.obs_param.getroot().find('body')
-                                                        .find('observed_level').text][step_index] * self.photon_fraction
-        return light_profile
+            if self.obs_profile[0][detector_index] + float(self.obs_param.getroot().find('body').find('detector_size').text)\
+                    / 2 < max(self.beamlet.profiles.beamlet_grid):
+                detector_steps = numpy.where(abs(self.beamlet.profiles.beamlet_grid-self.obs_profile[0][detector_index])
+                                             < float(self.obs_param.getroot().find('body').find('detector_size').text) / 2)
+                for step_index in detector_steps[0]:
+                    self.photon_emission_profile[detector_index] = self.photon_emission_profile[detector_index] + \
+                                                    self.beamlet.profiles[self.observed_level][step_index] * \
+                                                    self.photon_fraction
+        observing_detectors = numpy.where(self.photon_emission_profile != 0)[0]
+        print('Only detectors completely inside the calculated region could observe the virtual beamlet, these detector'
+              ' numbers are: ' + str(observing_detectors) + '.')
+        return
 
     @staticmethod
     def read_observation_param(data_path):
