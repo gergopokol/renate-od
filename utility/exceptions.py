4fe7daa2c4b962b29ff05885a59ce5e47b1580c3
asztalosors <asztalos@reak.bme.hu>
2018-10-16 20:42:38 +0200

    #18 Added exception dedicated for ids instnce loading.

--- utility/exceptions.py
+++ utility/exceptions.py
@@ -16,3 +16,13 @@ class IdsAttributeLoadError(Exception):
     def __new__(*args, **kwargs):
         """ Create and return a new object.  See help(type) for accurate signature. """
         pass
+
+
+class IdsInstanceLoadError(Exception):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @staticmethod
+    def __new__(*args, **kwargs):
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
