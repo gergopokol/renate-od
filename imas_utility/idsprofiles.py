b1dd5bbd8b02a823517ecd08fa7182b7193fe575
asztalosors <asztalos@reak.bme.hu>
2018-10-16 23:35:08 +0200

    #10 Applying error handling to plasma profile reading from ids

--- imas_utility/idsprofiles.py
+++ imas_utility/idsprofiles.py
@@ -1,5 +1,6 @@
 from imas_utility.idsinstance import ImasObject
 from utility.exceptions import IdsInstanceLoadError
+from utility.exceptions import IdsAttributeLoadError
 import numpy as np
 
 
@@ -12,10 +13,11 @@ class ProfilesIds(ImasObject):
         self.load_profiles_from_ids(source)
 
     def load_profiles_from_ids(self, source):
+        self.source = source
         try:
-            self.profiles = self.imas_pointer.get(source)
+            self.profiles = self.imas_pointer.get(self.source)
         except:
-            raise IdsInstanceLoadError('No ' + source + ' Ids was found for shot: ' 
+            raise IdsInstanceLoadError('No ' + self.source + ' IDS was found for shot: '
                                        + str(self.shot) + ' at run ' + str(self.run))
 
     def get_grid_in_rho_tor_norm(self, time):
@@ -23,56 +25,55 @@ class ProfilesIds(ImasObject):
         try:
             return self.profiles.profiles_1d[time_index].grid.rho_tor_norm
         except:
-            print('There is no available rho tor norm based grid for Shot:' +
-                  str(self.shot) + ' at Run: ' + str(self.run))
-            exit()
+            raise IdsAttributeLoadError('There is no available rho tor norm based grid for Shot:' +
+                                        str(self.shot) + ' at Run: ' + str(self.run) + ' in ' + self.source + ' IDS.')
 
     def get_grid_in_psi(self, time):
         time_index = self.get_time_index(time)
         try:
             return self.profiles.profiles_1d[time_index].grid.psi
         except:
-            print('There is no available psi based grid for Shot:' + str(self.shot) + ' at Run: ' + str(self.run))
-            exit()
+            raise IdsAttributeLoadError('There is no available psi based grid for Shot:' +
+                                        str(self.shot) + ' at Run: ' + str(self.run) + ' in ' + self.source + ' IDS.')
 
     def get_grid_in_rho_tor(self, time):
         time_index = self.get_time_index(time)
         try:
             return self.profiles.profiles_1d[time_index].grid.rho_tor
         except:
-            print('There is no available rho tor based grid for Shot:' + str(self.shot) + ' at Run: ' + str(self.run))
-            exit()
+            raise IdsAttributeLoadError('There is no available rho tor based grid for Shot:' +
+                                        str(self.shot) + ' at Run: ' + str(self.run) + ' in ' + self.source + ' IDS')
 
     def get_electron_density(self, time):
         time_index = self.get_time_index(time)
         try:
             return self.profiles.profiles_1d[time_index].electrons.density
         except:
-            print('There is no available electron density for Shot:' + str(self.shot) + ' at Run: ' + str(self.run))
-            exit()
+            raise IdsAttributeLoadError('There is no available electron density for Shot:' +
+                                        str(self.shot) + ' at Run: ' + str(self.run) + ' in ' + self.source + ' IDS')
 
     def get_electron_temperature(self, time):
         time_index = self.get_time_index(time)
         try:
             return self.profiles.profiles_1d[time_index].electrons.temperature
         except:
-            print('There is no available electron temperature for Shot:' + str(self.shot) + ' at Run: ' + str(self.run))
-            exit()
+            raise IdsAttributeLoadError('There is no available electron temperature for Shot:' +
+                                        str(self.shot) + ' at Run: ' + str(self.run) + ' in ' + self.source + ' IDS')
 
     def get_ion_temperature(self, time):
         time_index = self.get_time_index(time)
         try:
             return self.profiles.profiles_1d[time_index].ion[1].temperature
         except:
-            print('There is no available D temperature for Shot:' + str(self.shot) + ' at Run: ' + str(self.run))
-            exit()
+            raise IdsAttributeLoadError('There is no available D temperature for Shot:' +
+                                        str(self.shot) + ' at Run: ' + str(self.run) + ' in ' + self.source + ' IDS')
 
     def get_time_index(self, time):
         try:
             time_array = self.profiles.time
         except:
-            print('No time array available for the requested Shot: '+str(self.shot)+' and Run: '+str(self.run))
-            exit()
+            raise IdsAttributeLoadError('No time array available for the requested Shot: ' +
+                                        str(self.shot)+' and Run: '+str(self.run) + ' in ' + self.source + ' IDS')
 
         if (time_array[time_array.argmin()] <= time) and (time_array[time_array.argmax()] >= time):
             return (np.abs(time_array - time)).argmin()
