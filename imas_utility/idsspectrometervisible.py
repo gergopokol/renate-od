02ed099d153c68a418562a47321d3a699f045c40
asztalosors <asztalos@reak.bme.hu>
2018-10-17 00:07:31 +0200

    #10 Adding error handling to spectrometer_visible ids reading routines.

--- imas_utility/idsspectrometervisible.py
+++ imas_utility/idsspectrometervisible.py
@@ -1,4 +1,5 @@
 from imas_utility.idsinstance import ImasObject
+from utility.exceptions import IdsInstanceLoadError
 import numpy as np
 
 
@@ -11,6 +12,5 @@ class SpectrometerVisibleIds(ImasObject):
         try:
             self.spectro_vis = self.imas_pointer.get('spectrometer_visible')
         except:
-            print('The spectrometer_visible IDS is absent from data file. Muhaha.')
-            print('Please get more information about shot ' + str(self.shot) + ' at run ' + str(self.run))
-            exit()
+            raise IdsInstanceLoadError('No spectrometer_visible IDS was found for shot: '
+                                       + str(self.shot) + ' at run ' + str(self.run))
