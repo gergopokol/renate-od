f46cbb5304d341f5b1b96fafc28fc4fea7522b19
asztalosors <asztalos@reak.bme.hu>
2018-12-07 17:41:44 +0100

    #21 Adding the tests still to be implemented. Some tests are not implemented yet, other test without the assert statements still fail.

--- crm_solver/odetest.py
+++ crm_solver/odetest.py
@@ -12,10 +12,12 @@ class OdeTest(unittest.TestCase):
     END_INTERVAL = 0.1
     STEP_NUMBER = 100
     STEPS = numpy.linspace(START_INTERVAL, END_INTERVAL, STEP_NUMBER)
+    STEPS_VARYING_NONDIAGONAL = numpy.array([0, 0.1, 0.2, 0.3])
     START_POSITION = (STEPS[0] + STEPS[1]) / 2.
 
     INITIAL_CONDITION = numpy.array([1., 2.])
     INITIAL_CONDITION_CONSTANT_NONDIAGONAL = numpy.array([3, -11, 11])
+    INITIAL_CONDITION_VARYING_NONDIAGONAL = numpy.array([2, -1])
     INITIAL_CONDITION_1D = [numpy.array([0.]), numpy.array([1.])]
 
     COEFFICIENT_MATRIX_DIM_ERROR = numpy.array([15, 12])
@@ -26,8 +28,17 @@ class OdeTest(unittest.TestCase):
                                                            [-1, -3, 1]])
     COEFFICIENT_MATRIX_CHANGING = numpy.tensordot(COEFFICIENT_MATRIX_CONSTANT_DIAGONAL, STEPS, axes=0)
     COEFFICIENT_MATRIX_1D = [numpy.array([[2.]]), numpy.array([[2.]])]
+    COEFFICIENT_MATRIX_VARYING_NONDIAGONAL = numpy.zeros((2, 2, STEPS_VARYING_NONDIAGONAL.size))
+    COEFFICIENT_MATRIX_VARYING_NONDIAGONAL[:, :, 0] = numpy.array([[2, 3], [-1, -2]])
+    COEFFICIENT_MATRIX_VARYING_NONDIAGONAL[:, :, 1] = numpy.array([[2, 1], [2, -2]])
+    COEFFICIENT_MATRIX_VARYING_NONDIAGONAL[:, :, 2] = numpy.array([[2, -1], [5, -2]])
+    COEFFICIENT_MATRIX_VARYING_NONDIAGONAL[:, :, 3] = numpy.array([[2, -3], [8, -2]])
 
     EXPECTED_RESULT_CONSTANT_NONDIAGONAL = numpy.array([3.38999231881, -14.9942067896, 13.5292786755])
+    EXPECTED_RESULT_VARYING_NONDIAGONAL = numpy.array([[2, -1],
+                                                       [2.5108421, -0.20367017],
+                                                       [3.0526073,  0.0849155],
+                                                       [3.3920525, -1.1205913]])
     EXPECTED_SIZE_2 = 2
     EXPECTED_SIZE_100 = 100
     EXPECTED_SIZE_200 = 200
@@ -45,6 +56,9 @@ class OdeTest(unittest.TestCase):
         self.assertEqual(actual.size, self.EXPECTED_SIZE_2)
         for index in range(actual.size):
             self.assertAlmostEqual(actual[index], self.EXPECTED_DERIVATIVE_VECTOR_1[index], self.DECIMALS_6)
+
+    def test_setup_derivative_vector_for_constant_nondiagonal_case(self):
+        pass
 
     def test_setup_derivative_vector_with_coefficient_matrix_changing(self):
         ode = Ode(coefficient_matrix=self.COEFFICIENT_MATRIX_CHANGING,
@@ -91,6 +105,12 @@ class OdeTest(unittest.TestCase):
         for index in range(self.INITIAL_CONDITION_CONSTANT_NONDIAGONAL.size):
             self.assertAlmostEqual(actual[-1, index], self.EXPECTED_RESULT_CONSTANT_NONDIAGONAL[index], self.DECIMALS_6)
 
+    def test_calculate_integrate_solution_for_varying_nondiagonal_case(self):
+        ode = Ode(coefficient_matrix=self.COEFFICIENT_MATRIX_VARYING_NONDIAGONAL,
+                  initial_condition=self.INITIAL_CONDITION_VARYING_NONDIAGONAL,
+                  steps=self.STEPS_VARYING_NONDIAGONAL)
+        actual = ode.calculate_integrate_solution()
+
     def test_calculate_analytical_solution_1d(self):
         for init in self.INITIAL_CONDITION_1D:
             for coefficient in self.COEFFICIENT_MATRIX_1D:
@@ -114,6 +134,16 @@ class OdeTest(unittest.TestCase):
                                                      self.STEPS[i])
                 self.assertAlmostEqual(actual[i, j], expected, self.DECIMALS_6)
 
+    def test_calculate_analytical_solution_for_constant_nondiagnoal_case(self):
+        ode = Ode(coefficient_matrix=self.COEFFICIENT_MATRIX_CONSTANT_NONDIAGONAL,
+                  initial_condition=self.INITIAL_CONDITION_CONSTANT_NONDIAGONAL,
+                  steps=self.STEPS)
+        actual = ode.calculate_analytical_solution()
+        pass
+
+    def test_calculate_analytical_solution_for_varying_nondiagonal_case(self):
+        pass
+
     def test_benchmark_solvers_for_constant_diagonal_case(self):
         ode = Ode(coefficient_matrix=self.COEFFICIENT_MATRIX_CONSTANT_DIAGONAL,
                   initial_condition=self.INITIAL_CONDITION,
@@ -125,6 +155,12 @@ class OdeTest(unittest.TestCase):
         for i in range(self.STEP_NUMBER):
             for j in range(self.INITIAL_CONDITION.size):
                 self.assertAlmostEqual(integrate[i, j], analytical[i, j], self.DECIMALS_6)
+
+    def test_benchmark_solvers_for_constant_nondiagonal_case(self):
+        pass
+
+    def test_benchmark_solvers_for_varying_nondiagonal_case(self):
+        pass
 
     def test_almost_equal(self):
         actual = 1.004
