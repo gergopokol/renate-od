3bdbf210ba4f41ec93cd4a2a745139bc74158b5a
SzondyB <szondy.borbala@gmail.com>
2018-12-20 18:45:13 +0100

    #26 The simplified coefficient matrix is updated to handle rate coefficient arrays from the modificated rates.py

--- crm_solver/beamlet.py
+++ crm_solver/beamlet.py
@@ -72,14 +72,14 @@ class Beamlet:
         return
 
     def initialize_ode(self):
-        self.coefficient_matrix = CoefficientMatrix(self.param, self.profiles)
+        self.coefficient_matrix = CoefficientMatrix(self.param, self.imp_profiles, self.imp_components)
         self.initial_condition = [1] + [0] * (self.coefficient_matrix.number_of_levels - 1)
 
     def solve_numerically(self):
         if self.coefficient_matrix is None or self.initial_condition is None:
             self.initialize_ode()
         ode = Ode(coefficient_matrix=self.coefficient_matrix.matrix, initial_condition=self.initial_condition,
-                  steps=self.profiles['beamlet_grid'])
+                  steps=self.imp_profiles['beamlet_grid'])
         numerical = ode.calculate_integrate_solution()
         for level in range(self.coefficient_matrix.number_of_levels):
             label = 'level ' + str(level)
--- crm_solver/coefficientmatrix.py
+++ crm_solver/coefficientmatrix.py
@@ -3,9 +3,9 @@ from crm_solver.rates import Rates
 
 
 class CoefficientMatrix:
-    def __init__(self, beamlet_param, beamlet_profiles):
+    def __init__(self, beamlet_param, beamlet_profiles, plasma_components):
         self.beamlet_profiles = beamlet_profiles
-        self.rates = Rates(beamlet_param, beamlet_profiles)
+        self.rates = Rates(beamlet_param, beamlet_profiles, plasma_components)
         self.number_of_steps = self.rates.number_of_steps
         self.number_of_levels = self.rates.number_of_levels
         # Initialize matrices
@@ -30,25 +30,25 @@ class CoefficientMatrix:
                             - sum(self.rates.electron_neutral_collisions[from_level, :to_level, step]) \
                             - sum(self.rates.electron_neutral_collisions[from_level, (to_level+1):self.number_of_levels,
                                   step]) \
-                            - self.rates.electron_loss_collisions[0, from_level, step]
+                            - self.rates.electron_loss_collisions[from_level, step]
                         self.ion_terms[from_level, to_level, step] = \
-                            - sum(self.rates.proton_neutral_collisions[from_level, :to_level, step]) \
-                            - sum(self.rates.proton_neutral_collisions[from_level, (to_level+1):self.number_of_levels,
+                            - sum(self.rates.ion_neutral_collisions[0][from_level, :to_level, step]) \
+                            - sum(self.rates.ion_neutral_collisions[0][from_level, (to_level+1):self.number_of_levels,
                                   step]) \
-                            - self.rates.electron_loss_collisions[1, from_level, step]
+                            - self.rates.electron_loss_ion_collisions[0][from_level, step]
                         self.photon_terms[from_level, to_level, step] = \
                             - sum(self.rates.einstein_coeffs[:, from_level]) / self.rates.velocity
                     else:
                         self.electron_terms[from_level, to_level, step] = \
                             self.rates.electron_neutral_collisions[from_level, to_level, step]
                         self.ion_terms[from_level, to_level, step] = \
-                            self.rates.proton_neutral_collisions[from_level, to_level, step]
+                            self.rates.ion_neutral_collisions[0][from_level, to_level, step]
                         self.photon_terms[from_level, to_level, step] = \
                             self.rates.einstein_coeffs[to_level, from_level] / self.rates.velocity
 
     def apply_density(self):
         for step in range(self.number_of_steps):
-            self.matrix[:, :, step] = self.beamlet_profiles['beamlet_density'][step] * self.electron_terms[:, :, step] \
-                                      + self.beamlet_profiles['beamlet_density'][step] * self.ion_terms[:, :, step] \
+            self.matrix[:, :, step] = self.beamlet_profiles['electron']['density'][step] * self.electron_terms[:, :, step] \
+                                      + self.beamlet_profiles['ion1']['density'][step] * self.ion_terms[:, :, step] \
                                       + self.photon_terms[:, :, step]
 
