a4f6396b72c17656e9cfd9720564a2faaeb7fb8c
mig8 <mindler.gergely@gmail.com>
2019-01-27 21:14:13 +0100

    rename set_derivative_vector and coeff relevant vars in it

--- crm_solver/ode.py
+++ crm_solver/ode.py
@@ -9,7 +9,7 @@ class Ode:
         self.init_condition = init_condition
 
     def calculate_numerical_solution(self, steps):
-        return odeint(func=self.setup_derivative_vector, y0=self.init_condition, t=steps,
+        return odeint(func=self.set_derivative_vector, y0=self.init_condition, t=steps,
                       args=(self.coeff_matrix, steps))
 
     def calculate_analytical_solution(self, steps):
@@ -37,15 +37,15 @@ class Ode:
         return coeff * numpy.exp(eigenvalues * step_item)
 
     @staticmethod
-    def setup_derivative_vector(variable_vector, actual_position, coefficient_matrix, steps):
-        if coefficient_matrix.ndim == 3:
-            interp_coefficient_matrix = interp1d(steps, coefficient_matrix, axis=2, fill_value='extrapolate')
-            return numpy.dot(variable_vector, interp_coefficient_matrix(actual_position))
-        elif coefficient_matrix.ndim == 2:
-            return numpy.dot(variable_vector, coefficient_matrix)
+    def set_derivative_vector(variable_vector, actual_position, coeff_matrix, steps):
+        if coeff_matrix.ndim == 3:
+            interp_coeff_matrix = interp1d(steps, coeff_matrix, axis=2, fill_value='extrapolate')
+            return numpy.dot(variable_vector, interp_coeff_matrix(actual_position))
+        elif coeff_matrix.ndim == 2:
+            return numpy.dot(variable_vector, coeff_matrix)
         else:
             raise ValueError(
-                'Rate Coefficient Matrix of dimensions: ' + str(coefficient_matrix.ndim) + ' is not supported')
+                'Rate Coefficient Matrix of dimensions: ' + str(coeff_matrix.ndim) + ' is not supported')
 
     @staticmethod
     def calculate_1d_solution(init, coefficient, variable):
