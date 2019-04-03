c33375a31ac748200a2f3d6f2de92f0137cf1c5c
asztalosors <asztalos@reak.bme.hu>
2019-04-03 16:42:12 +0200

    #42 Correcting errors in constant table and cleaning it up.

--- utility/constants.py
+++ utility/constants.py
@@ -1,7 +1,4 @@
 class Constants:
     def __init__(self):
-        self.charge_electron = 1.602176487e-16   #[C]
-        self.mass = 1.1525*1e-26
+        self.charge_electron = 1.602176487e-19   #[C]
         self.speed_of_light = 299792458       #[m / s]
-        self.einstein = 3.72*1e7
-
