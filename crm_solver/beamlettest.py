d4b3a79fbcdbf43cd0caa9212a2ac7752bc55898
asztalosors <asztalos@reak.bme.hu>
2019-05-13 22:21:10 +0200

    #50 Adding beamket unittest file. Purpose is to have an automated set of tests benchmarking RENATE to RENATE-OD

---
+++ crm_solver/beamlettest.py
@@ -0,0 +1,20 @@
+import unittest
+import numpy
+from crm_solver.beamlet import Beamlet
+
+
+class BeamletTest(unittest.TestCase):
+
+    DECIMALS_10 = 10
+
+    def test_lithium_beamlet(self):
+        # TODO: Implement code to benchmark lithium beam evolution: RENATE with RENATE-OD
+        pass
+
+    def test_sodium_beamlet(self):
+        # TODO: Implement code to benchmark sodium beam evolution: RENATE with RENATE-OD
+        pass
+
+    def test_hydrogen_beamlet(self):
+        # TODO: Implement code to benchmark hydrogen beam evolution: RENATE with RENATE-OD
+        pass
