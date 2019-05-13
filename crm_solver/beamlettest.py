00f37a006f310a962dd1c51cb85d6b89206c5e9d
asztalosors <asztalos@reak.bme.hu>
2019-05-14 01:36:40 +0200

    #50 Adding test case calculations.

--- crm_solver/beamlettest.py
+++ crm_solver/beamlettest.py
@@ -1,23 +1,30 @@
 import unittest
 import numpy
+import numpy.testing as npt
 from crm_solver.beamlet import Beamlet
 
 
 class BeamletTest(unittest.TestCase):
 
-    DECIMALS_6 = 6
-    Lithium_testcase = 'D_plasma_Li_beam@60keV.xml'
-    Sodium_testcase = 'H_plasma_Na_beam@60keV.xml'
-    Hydrogen_testcase = 'He_plasma_H_beam@60keV.xml'
+    DECIMALS_PRECISSION = 1
+    lithium_testcase = 'beamlet/D_plasma_Li_beam@60keV.xml'
+    sodium_testcase = 'beamlet/H_plasma_Na_beam@60keV.xml'
+    hydrogen_testcase = 'beamlet/He_plasma_H_beam@60keV.xml'
 
     def test_lithium_beamlet(self):
-        # TODO: Implement code to benchmark lithium beam evolution: RENATE with RENATE-OD
-        pass
+        actual = Beamlet(data_path=self.lithium_testcase)
+        levels = actual.profiles.filter(like='RENATE', axis=1)
+        nr_levels = len(levels.keys())
+        #for level in range(nr_levels):
+        #    npt.assert_almost_equal(actual.profiles['level ' + str(level)],
+        #                            actual.profiles['level ' + str(level)], self.DECIMALS_PRECISSION)
 
     def test_sodium_beamlet(self):
-        # TODO: Implement code to benchmark sodium beam evolution: RENATE with RENATE-OD
-        pass
+        actual = Beamlet(data_path=self.sodium_testcase)
+        levels = actual.profiles.filter(like='RENATE', axis=1)
+        nr_levels = len(levels.keys())
 
     def test_hydrogen_beamlet(self):
-        # TODO: Implement code to benchmark hydrogen beam evolution: RENATE with RENATE-OD
-        pass
+        actual = Beamlet(data_path=self.hydrogen_testcase)
+        levels = actual.profiles.filter(like='RENATE', axis=1)
+        nr_levels = len(levels.keys())
