d59c748cc6502942021de06ab184f268a77d031e
asztalosors <asztalos@reak.bme.hu>
2019-05-14 00:01:56 +0200

    #50 Adding test case descriptions.

--- crm_solver/beamlettest.py
+++ crm_solver/beamlettest.py
@@ -5,7 +5,10 @@ from crm_solver.beamlet import Beamlet
 
 class BeamletTest(unittest.TestCase):
 
-    DECIMALS_10 = 10
+    DECIMALS_6 = 6
+    Lithium_testcase = 'D_plasma_Li_beam@60keV.xml'
+    Sodium_testcase = 'H_plasma_Na_beam@60keV.xml'
+    Hydrogen_testcase = 'He_plasma_H_beam@60keV.xml'
 
     def test_lithium_beamlet(self):
         # TODO: Implement code to benchmark lithium beam evolution: RENATE with RENATE-OD
