05499451c08843280689c42a4fbd48bbd8273345
asztalosors <asztalos@reak.bme.hu>
2019-05-14 02:40:23 +0200

    #50 Implemented test cases. NAN error still exists.

--- crm_solver/beamlettest.py
+++ crm_solver/beamlettest.py
@@ -6,7 +6,7 @@ from crm_solver.beamlet import Beamlet
 
 class BeamletTest(unittest.TestCase):
 
-    DECIMALS_PRECISSION = 1
+    DECIMALS_PRECISSION = 0
     lithium_testcase = 'beamlet/D_plasma_Li_beam@60keV.xml'
     sodium_testcase = 'beamlet/H_plasma_Na_beam@60keV.xml'
     hydrogen_testcase = 'beamlet/He_plasma_H_beam@60keV.xml'
@@ -15,16 +15,29 @@ class BeamletTest(unittest.TestCase):
         actual = Beamlet(data_path=self.lithium_testcase)
         levels = actual.profiles.filter(like='RENATE', axis=1)
         nr_levels = len(levels.keys())
-        #for level in range(nr_levels):
-        #    npt.assert_almost_equal(actual.profiles['level ' + str(level)],
-        #                            actual.profiles['level ' + str(level)], self.DECIMALS_PRECISSION)
+        for level in range(nr_levels):
+            renate = actual.profiles['RENATE level ' + str(level)]
+            renate_od = actual.profiles['level ' + str(level)]/actual.profiles['level 0'].max()
+            for index in range(len(renate)):
+                self.assertAlmostEqual(renate[index], renate_od[index], self.DECIMALS_PRECISSION)
+
 
     def test_sodium_beamlet(self):
         actual = Beamlet(data_path=self.sodium_testcase)
         levels = actual.profiles.filter(like='RENATE', axis=1)
         nr_levels = len(levels.keys())
+        for level in range(nr_levels):
+            renate = actual.profiles['RENATE level ' + str(level)]
+            renate_od = actual.profiles['level ' + str(level)]/actual.profiles['level 0'].max()
+            for index in range(len(renate)):
+                self.assertAlmostEqual(renate[index], renate_od[index], self.DECIMALS_PRECISSION)
 
     def test_hydrogen_beamlet(self):
         actual = Beamlet(data_path=self.hydrogen_testcase)
         levels = actual.profiles.filter(like='RENATE', axis=1)
         nr_levels = len(levels.keys())
+        for level in range(nr_levels):
+            renate = actual.profiles['RENATE level ' + str(level)]
+            renate_od = actual.profiles['level ' + str(level)]/actual.profiles['level 0'].max()
+            for index in range(len(renate)):
+                self.assertAlmostEqual(renate[index], renate_od[index], self.DECIMALS_PRECISSION)
