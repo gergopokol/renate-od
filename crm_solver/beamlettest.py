35d12a174868386a2f4845b3f33e6e405826be4a
asztalosors <asztalos@reak.bme.hu>
2019-06-14 19:37:44 +0200

    #50 Adding correction to unit testing. Failure is set at 1% relative error between RENATE and RENATE-OD. Currently all test are failing.

--- crm_solver/beamlettest.py
+++ crm_solver/beamlettest.py
@@ -4,7 +4,7 @@ from crm_solver.beamlet import Beamlet
 
 class BeamletTest(unittest.TestCase):
 
-    DECIMALS_PRECISSION = 0
+    RELATIVE_PRECISION = 0.01
     lithium_testcase = 'beamlet/D_plasma_Li_beam@60keV.xml'
     sodium_testcase = 'beamlet/H_plasma_Na_beam@60keV.xml'
     hydrogen_testcase = 'beamlet/He_plasma_H_beam@60keV.xml'
@@ -17,8 +17,7 @@ class BeamletTest(unittest.TestCase):
             renate = actual.profiles['RENATE level ' + str(level)]
             renate_od = actual.profiles['level ' + str(level)]/actual.profiles['level 0'].max()
             for index in range(len(renate)-1):
-                self.assertAlmostEqual(renate[index], renate_od[index], self.DECIMALS_PRECISSION)
-
+                self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
 
     def test_sodium_beamlet(self):
         actual = Beamlet(data_path=self.sodium_testcase)
@@ -28,7 +27,7 @@ class BeamletTest(unittest.TestCase):
             renate = actual.profiles['RENATE level ' + str(level)]
             renate_od = actual.profiles['level ' + str(level)]/actual.profiles['level 0'].max()
             for index in range(len(renate)-1):
-                self.assertAlmostEqual(renate[index], renate_od[index], self.DECIMALS_PRECISSION)
+                self.assertTrue(abs(renate[index] - renate_od[index]) / renate[index] <= self.RELATIVE_PRECISION)
 
     def test_hydrogen_beamlet(self):
         actual = Beamlet(data_path=self.hydrogen_testcase)
@@ -38,4 +37,4 @@ class BeamletTest(unittest.TestCase):
             renate = actual.profiles['RENATE level ' + str(level)]
             renate_od = actual.profiles['level ' + str(level)]/actual.profiles['level 0'].max()
             for index in range(len(renate)-1):
-                self.assertAlmostEqual(renate[index], renate_od[index], self.DECIMALS_PRECISSION)
+                self.assertTrue(abs(renate[index] - renate_od[index]) / renate[index] <= self.RELATIVE_PRECISION)
