b252460fa58d490401ef4b9b00a444d8df241a48
asztalosors <asztalos@reak.bme.hu>
2019-06-17 13:45:50 +0200

    #56 Changing some of the naming convention.

--- crm_solver/atomi_db.py
+++ crm_solver/atomi_db.py
@@ -9,20 +9,20 @@ class AtomicDB:
         if not isinstance(self.param, etree._ElementTree):
             self.param = GetData(data_path_name=data_path).data
         assert isinstance(self.param, etree._ElementTree)
-        self.beam_energy = self.param.getroot().find('body').find('beamlet_energy').text
-        self.beam_species = self.param.getroot().find('body').find('beamlet_species').text
+        self.energy = self.param.getroot().find('body').find('beamlet_energy').text
+        self.species = self.param.getroot().find('body').find('beamlet_species').text
         self.__set_atomic_dictionary()
 
     def __set_atomic_dictionary(self):
-        assert isinstance(self.beam_species, str)
-        if self.beam_species not in ['H', 'D', 'T', 'Li', 'Na']:
-            raise Exception(self.beam_species + ' beam atomic data not supported')
-        if self.beam_species in ['H', 'D', 'T']:
+        assert isinstance(self.species, str)
+        if self.species not in ['H', 'D', 'T', 'Li', 'Na']:
+            raise Exception(self.species + ' beam atomic data not supported')
+        if self.species in ['H', 'D', 'T']:
             self.atomic_dict = {'1n': 0, '2n': 1, '3n': 2, '4n': 3, '5n': 4, '6n': 5}
             self.atomic_levels = 6
-        if self.beam_species is 'Li':
+        if self.species is 'Li':
             self.atomic_dict = {'2s': 0, '2p': 1, '3s': 2, '3p': 3, '3d': 4, '4s': 5, '4p': 6, '4d': 7, '4f': 8}
             self.atomic_levels = 9
-        if self.beam_species is 'Na':
+        if self.species is 'Na':
             self.atomic_dict = {'3s': 0, '3p': 1, '3d': 2, '4s': 3, '4p': 4, '4d': 5, '4f': 6, '5s': 7}
             self.atomic_levels = 8
