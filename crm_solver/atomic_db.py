4e34267d6c85c834c61e8fa672ee69b4f0054d88
asztalosors <asztalos@reak.bme.hu>
2019-06-19 15:06:11 +0200

    #56 Changing atomic physics annotation for bundled-n calling sequence: 1n, 2n, ... 6n --> 1, 2, ... 6. All are strings

--- crm_solver/atomic_db.py
+++ crm_solver/atomic_db.py
@@ -19,7 +19,7 @@ class AtomicDB:
 
     def set_default_atomic_levels(self):
         if self.species in ['H', 'D', 'T']:
-            return '3n', '2n', '1n', '3n-2n'
+            return '3', '2', '1', '3-2'
         elif self.species == 'Li':
             return '2p', '2s', '2s', '2p-2s'
         elif self.species == 'Na':
@@ -97,7 +97,7 @@ class AtomicDB:
         if self.species not in ['H', 'D', 'T', 'Li', 'Na']:
             raise Exception(self.species + ' beam atomic data not supported')
         if self.species in ['H', 'D', 'T']:
-            self.atomic_dict = {'1n': 0, '2n': 1, '3n': 2, '4n': 3, '5n': 4, '6n': 5}
+            self.atomic_dict = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5}
             self.atomic_levels = 6
         if self.species == 'Li':
             self.atomic_dict = {'2s': 0, '2p': 1, '3s': 2, '3p': 3, '3d': 4, '4s': 5, '4p': 6, '4d': 7, '4f': 8}
