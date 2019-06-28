7d18fc0314a690c330bd386d05ba45142862e3c9
SzondyB <szondy.borbala@gmail.com>
2019-06-28 19:16:09 +0200

    #8 Observation module updated. ToDo: Consistent beamlet and observation profile test files have to be created.

--- observation/observation.py
+++ observation/observation.py
@@ -1,40 +1,47 @@
 from lxml import etree
 import pandas
 import numpy
-import math
 from utility.constants import Constants
-from utility import convert
 from utility import getdata
-import re
-from pdb import set_trace as bp
+import warnings
+
 
 
 class Obs1d:
-    def __init__(self, beamlet, data_path, beam_current):
+    def __init__(self, beamlet, data_path, from_level=None, to_level=None):
         self.beamlet = beamlet
         self.constants = Constants()
         self.obs_param = self.read_observation_param(data_path)
-        self.obs_profile = self.read_observation_profile(data_path=self.obs_param.getroot().find('body').find('observation_profile_path').text)
-        self.observed_level = self.obs_param.getroot().find('body').find('observed_level').text
-        self.observed_level_index = int(re.findall('\\d+', self.observed_level)[0])
-        self.photon_fraction = beam_current * self.beamlet.coefficient_matrix.spontaneous_trans_np[0, self.observed_level_index]\
-                               / self.constants.charge_electron / float(beamlet.param.getroot().find('body').find('beamlet_velocity').text)
+        self.obs_profile = self.read_observation_profile(data_path=self.obs_param.getroot().find('body').find(
+            'observation_profile_path').text)
+        self.detector_size = float(self.obs_param.getroot().find('body').find('detector_size').text)
+        if (max([self.obs_profile[0][i+1]-self.obs_profile[0][i] for i in range(self.obs_profile[0].size-1)]) >
+                min([self.beamlet.profiles['beamlet grid']['distance']['m'][j + 1] -
+                 self.beamlet.profiles['beamlet grid']['distance']['m'][j] for j in range(
+                self.beamlet.profiles['beamlet grid']['distance']['m'].size - 1)])):
+            warnings.warn('The calculated resolution is poorer than the detector resolution. It is recommended to rerun the '
+                  'photon emission calculations on a finer grid. If you still want to continue, '
+                  'call calculate_photon_emission_profile() function.')
+
+        if (from_level is None) or (to_level is None):
+            self.observed_level = beamlet.atomic_db.set_default_atomic_levels()[3]
+        else:
+            self.observed_level = str(from_level) + '-' + str(to_level)
+        beamlet.compute_linear_emission_density(from_level, to_level)
         self.photon_emission_profile = numpy.zeros(self.obs_profile.size)
         self.emission_profile = pandas.DataFrame()
 
     def calculate_photon_emission_profile(self):
         for detector_index in range(len(self.obs_profile)):
-            if self.obs_profile[0][detector_index] + float(self.obs_param.getroot().find('body').find(
-                    'detector_size').text) / 2 < max(self.beamlet.profiles['beamlet grid']):
-                detector_steps = numpy.where(abs(self.beamlet.profiles['beamlet grid']-self.obs_profile[0][detector_index])
-                                             < float(self.obs_param.getroot().find('body').find('detector_size').text) / 2)
-                for step_index in detector_steps[0]:
-                    self.photon_emission_profile[detector_index] = self.photon_emission_profile[detector_index] + \
-                                                    self.beamlet.profiles[self.observed_level][step_index] * \
-                                                    self.photon_fraction
-        observing_detectors = numpy.where(self.photon_emission_profile != 0)[0]
-        self.emission_profile = pandas.concat([self.obs_profile, pandas.DataFrame(self.photon_emission_profile)], axis=1,
-                                              keys=['Location', 'Emission'])
+            if self.obs_profile[0][detector_index] + self.detector_size / 2. < max(self.beamlet.profiles['beamlet grid']['distance']['m']):
+                detector_steps = [i for i in range(self.beamlet.profiles['beamlet grid']['distance']['m'].size) if abs(
+                    self.beamlet.profiles['beamlet grid']['distance']['m'][i] - self.obs_profile[0][detector_index])
+                                       < (self.detector_size/2)]
+                self.photon_emission_profile[detector_index] = sum([self.beamlet.profiles[self.observed_level][step]
+                                                                    for step in detector_steps])
+        observing_detectors = numpy.nonzero(self.photon_emission_profile)
+        self.emission_profile = pandas.concat([self.obs_profile, pandas.DataFrame(self.photon_emission_profile)],
+                                              axis=1, keys=['Location', 'Emission'])
         print('Only detectors completely inside the calculated region could observe the virtual beamlet, these detector'
               ' numbers are: ' + str(observing_detectors) + '.')
         return
