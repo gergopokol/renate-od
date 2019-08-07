708f4e044868e330796d4842217df5201c740e0f
asztalosors <asztalos@reak.bme.hu>
2019-08-07 18:31:14 +0200

    #58 Adding testing routines to atomic db unittest.

--- crm_solver/atomic_dbtest.py
+++ crm_solver/atomic_dbtest.py
@@ -3,4 +3,18 @@ import unittest
 
 
 class AtomicDBTest(unittest.TestCase):
-    pass
+    EXPECTED_ATTR = ['energy', 'param', 'species', 'electron_impact_loss', 'ion_impact_loss',
+                     'atomic_dict', 'rate_type', 'electron_impact_trans', 'ion_impact_trans',
+                     'spontaneous_trans', 'atomic_levels', 'charged_states', 'inv_atomic_dict']
+    EXPECTED_ATOM = ['dummy', 'Li', 'Na', 'T', 'H', 'D']
+
+    def test_all_attributes(self):
+        actual = AtomicDB()
+        for attr in self.EXPECTED_ATTR:
+            assert hasattr(actual, attr)
+
+    def test_atomic_species(self):
+        actual = AtomicDB()
+        assert isinstance(actual.species, str)
+        assert actual.species in self.EXPECTED_ATOM
+
