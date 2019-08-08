c2b22dd3bb0be38d63a2862ae55d08b933193436
asztalosors <asztalos@reak.bme.hu>
2019-08-08 16:43:45 +0200

    #58 Correcting formalism of atomic species test. All test work.

--- crm_solver/atomic_dbtest.py
+++ crm_solver/atomic_dbtest.py
@@ -10,6 +10,12 @@ class AtomicDBTest(unittest.TestCase):
     EXPECTED_ATOM = ['dummy', 'Li', 'Na', 'T', 'H', 'D']
     EXPECTED_ATOMIC_LEVELS = [3, 9, 8, 6, 6, 6]
     EXPECTED_ENERGY = '60'
+    EXPECTED_ATOMIC_DICT = [{'1': 1, '0': 0, '2': 2},
+                            {'2s': 0, '2p': 1, '3s': 2, '3p': 3, '3d': 4, '4s': 5, '4p': 6, '4d': 7, '4f': 8},
+                            {'3s': 0, '3p': 1, '3d': 2, '4s': 3, '4p': 4, '4d': 5, '4f': 6, '5s': 7},
+                            {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5},
+                            {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5},
+                            {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5}]
 
     def test_all_attributes(self):
         actual = AtomicDB()
@@ -18,9 +24,9 @@ class AtomicDBTest(unittest.TestCase):
 
     def test_atomic_species(self):
         actual = AtomicDB()
-        assert isinstance(actual.species, str)
-        assert actual.species in self.EXPECTED_ATOM
-        assert isinstance(actual.species, str)
+        self.assertIsInstance(actual.species, str)
+        self.assertIn(actual.species, self.EXPECTED_ATOM)
+        self.assertIsInstance(actual.species, str)
 
     def test_atomic_levels(self):
         actual = AtomicDB()
