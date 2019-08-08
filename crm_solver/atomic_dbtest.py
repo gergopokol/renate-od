c0d648384103d939c9c8af93ab7d9e46167a5232
asztalosors <asztalos@reak.bme.hu>
2019-08-08 15:54:25 +0200

    #58 Adding tests for atomic label and data setting. Still fails due to lack of T beam data @ 60keV. Will upload correct data ASAP.

--- crm_solver/atomic_dbtest.py
+++ crm_solver/atomic_dbtest.py
@@ -1,5 +1,6 @@
 from crm_solver.atomic_db import AtomicDB
 import unittest
+from lxml import etree
 
 
 class AtomicDBTest(unittest.TestCase):
@@ -7,6 +8,8 @@ class AtomicDBTest(unittest.TestCase):
                      'atomic_dict', 'rate_type', 'electron_impact_trans', 'ion_impact_trans',
                      'spontaneous_trans', 'atomic_levels', 'charged_states', 'inv_atomic_dict']
     EXPECTED_ATOM = ['dummy', 'Li', 'Na', 'T', 'H', 'D']
+    EXPECTED_ATOMIC_LEVELS = [3, 9, 8, 6, 6, 6]
+    EXPECTED_ENERGY = '60'
 
     def test_all_attributes(self):
         actual = AtomicDB()
@@ -17,4 +20,14 @@ class AtomicDBTest(unittest.TestCase):
         actual = AtomicDB()
         assert isinstance(actual.species, str)
         assert actual.species in self.EXPECTED_ATOM
+        assert isinstance(actual.species, str)
 
+    def test_atomic_levels(self):
+        actual = AtomicDB()
+        assert isinstance(actual.atomic_dict, dict)
+        assert isinstance(actual.inv_atomic_dict, dict)
+        assert isinstance(actual.atomic_levels, int)
+        for index in range(len(self.EXPECTED_ATOM)):
+            actual.param.getroot().find('body').find('beamlet_species').text = self.EXPECTED_ATOM[index]
+            atom = AtomicDB(param=actual.param)
+            self.assertEqual(atom.atomic_levels, self.EXPECTED_ATOMIC_LEVELS[index])
