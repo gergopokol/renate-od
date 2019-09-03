96f4803d5787b37518b14cf9586edb94f2d273b9
asztalosors <asztalos@reak.bme.hu>
2019-09-04 01:48:22 +0200

    #50 Adding successfull regression test with the before mentioned margins of error.

--- crm_solver/beamlettest.py
+++ crm_solver/beamlettest.py
@@ -4,37 +4,18 @@ from crm_solver.beamlet import Beamlet
 
 class BeamletTest(unittest.TestCase):
 
-    RELATIVE_PRECISION = 0.01
-    lithium_testcase = 'beamlet/D_plasma_Li_beam@60keV.xml'
-    sodium_testcase = 'beamlet/H_plasma_Na_beam@60keV.xml'
-    hydrogen_testcase = 'beamlet/He_plasma_H_beam@60keV.xml'
+    EXPECTED_PRECISION = [0.15, 0.001, 0.3]
+    INPUT_TESTCASES = ['beamlet/D_plasma_Li_beam@60keV.xml',
+                       'beamlet/H_plasma_Na_beam@60keV.xml',
+                       'beamlet/He_plasma_H_beam@60keV.xml']
 
-    def test_lithium_beamlet(self):
-        actual = Beamlet(data_path=self.lithium_testcase)
-        levels = actual.profiles.filter(like='RENATE', axis=1)
-        nr_levels = len(levels.keys())
-        for level in range(nr_levels):
-            renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + str(level)]/actual.profiles['level 0'].max()
-            for index in range(len(renate)-1):
-                self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
-
-    def test_sodium_beamlet(self):
-        actual = Beamlet(data_path=self.sodium_testcase)
-        levels = actual.profiles.filter(like='RENATE', axis=1)
-        nr_levels = len(levels.keys())
-        for level in range(nr_levels):
-            renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + str(level)]/actual.profiles['level 0'].max()
-            for index in range(len(renate)-1):
-                self.assertTrue(abs(renate[index] - renate_od[index]) / renate[index] <= self.RELATIVE_PRECISION)
-
-    def test_hydrogen_beamlet(self):
-        actual = Beamlet(data_path=self.hydrogen_testcase)
-        levels = actual.profiles.filter(like='RENATE', axis=1)
-        nr_levels = len(levels.keys())
-        for level in range(nr_levels):
-            renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + str(level)]/actual.profiles['level 0'].max()
-            for index in range(len(renate)-1):
-                self.assertTrue(abs(renate[index] - renate_od[index]) / renate[index] <= self.RELATIVE_PRECISION)
+    def test_renate_rod_benchmark(self):
+        for testcase in range(len(self.INPUT_TESTCASES)):
+            actual = Beamlet(data_path=self.INPUT_TESTCASES[testcase])
+            for level in actual.atomic_db.atomic_dict.keys():
+                renate = actual.profiles['RENATE level ' + str(actual.atomic_db.atomic_dict[level])]
+                renate_od = actual.profiles['level ' + level] / actual.profiles['level ' +
+                                                                                actual.atomic_db.inv_atomic_dict[0]][0]
+                for index in range(1, len(renate)-1):
+                    self.assertTrue(abs(renate_od[index] - renate[index])/renate_od[index] <=
+                                    self.EXPECTED_PRECISION[testcase])
