ef4c071fc2e6bc9f248255c816b9c7a2757f6ad7
asztalosors <asztalos@reak.bme.hu>
2019-09-03 18:45:35 +0200

    #72 Adding expected values for Li 60 test case calculations.

--- crm_solver/atomic_dbtest.py
+++ crm_solver/atomic_dbtest.py
@@ -11,6 +11,8 @@ class AtomicDBTest(unittest.TestCase):
     EXPECTED_ATOM = ['dummy', 'Li', 'Na', 'T', 'H', 'D']
     EXPECTED_ATOMIC_LEVELS = [3, 9, 8, 6, 6, 6]
     EXPECTED_ENERGY = '60'
+    EXPECTED_VELOCITY = 1291547.1348
+    EXPECTED_MASS = 1.15258e-26
     EXPECTED_ATOMIC_DICT = [{'1': 1, '0': 0, '2': 2},
                             {'2s': 0, '2p': 1, '3s': 2, '3p': 3, '3d': 4, '4s': 5, '4p': 6, '4d': 7, '4f': 8},
                             {'3s': 0, '3p': 1, '3d': 2, '4s': 3, '4p': 4, '4d': 5, '4f': 6, '5s': 7},
@@ -104,6 +106,12 @@ class AtomicDBTest(unittest.TestCase):
     def test_projectile_mass(self):
         actual = AtomicDB()
         self.assertIsInstance(actual.mass, float)
+        self.assertEqual(actual.mass, self.EXPECTED_MASS)
+
+    def test_projectile_velocity(self):
+        actual = AtomicDB()
+        self.assertIsInstance(actual.velocity, float)
+        self.assertAlmostEqual(actual.velocity, self.EXPECTED_VELOCITY, 3)
 
     def test_atomic_species(self):
         actual = AtomicDB()
