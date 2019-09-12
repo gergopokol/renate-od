a83080a9d34a4b24ad313c23c0d5d5ade837b37c
asztalosors <asztalos@reak.bme.hu>
2019-09-12 16:25:50 +0200

    #84 Cleaned up acceptance test.

--- crm_solver/crmacceptancetest.py
+++ crm_solver/crmacceptancetest.py
@@ -4,7 +4,7 @@ from crm_solver.beamlet import Beamlet
 
 class CrmAcceptanceTest(unittest.TestCase):
 
-    RELATIVE_PRECISION = 0.01
+    RELATIVE_PRECISION = 0.001
     lithium_testcase = 'beamlet/acceptancetest/scenario-standard_plasma-H_energy-100_beam-Li_profile.xml'
     sodium_testcase = 'beamlet/acceptancetest/scenario-standard_plasma-H_energy-100_beam-Na_profile.xml'
     hydrogen_testcase = 'beamlet/acceptancetest/scenario-standard_plasma-H_energy-100_beam-H_profile.xml'
@@ -13,55 +13,50 @@ class CrmAcceptanceTest(unittest.TestCase):
 
     def test_lithium_beamlet(self):
         actual = Beamlet(data_path=self.lithium_testcase)
-        levels = actual.profiles.filter(like='RENATE', axis=1)
-        nr_levels = len(levels.keys())
-        for level in range(nr_levels):
+        for level in range(actual.atomic_db.atomic_levels):
             renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]]/actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
+            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] / \
+                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
             for index in range(len(renate)-1):
-                if renate[index]!=0:
+                if renate[index] != 0:
                     self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
 
     def test_sodium_beamlet(self):
         actual = Beamlet(data_path=self.sodium_testcase)
-        levels = actual.profiles.filter(like='RENATE', axis=1)
-        nr_levels = len(levels.keys())
-        for level in range(nr_levels):
+        for level in range(actual.atomic_db.atomic_levels):
             renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]]/actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
+            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] / \
+                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
             for index in range(len(renate)-1):
-                if renate[index]!=0:
+                if renate[index] != 0:
                     self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
 
     def test_hydrogen_beamlet(self):
         actual = Beamlet(data_path=self.hydrogen_testcase)
-        levels = actual.profiles.filter(like='RENATE', axis=1)
-        nr_levels = len(levels.keys())
-        for level in range(nr_levels):
+        for level in range(actual.atomic_db.atomic_levels):
             renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]]/actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
+            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] /\
+                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
             for index in range(len(renate)-1):
-                if renate[index]!=0:
+                if renate[index] != 0:
                     self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
 
     def test_deuterium_beamlet(self):
         actual = Beamlet(data_path=self.deuterium_testcase)
-        levels = actual.profiles.filter(like='RENATE', axis=1)
-        nr_levels = len(levels.keys())
-        for level in range(nr_levels):
+        for level in range(actual.atomic_db.atomic_levels):
             renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]]/actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
+            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] / \
+                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
             for index in range(len(renate)-1):
-                if renate[index]!=0:
+                if renate[index] != 0:
                     self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
 
     def test_tritium_beamlet(self):
         actual = Beamlet(data_path=self.tritium_testcase)
-        levels = actual.profiles.filter(like='RENATE', axis=1)
-        nr_levels = len(levels.keys())
-        for level in range(nr_levels):
+        for level in range(actual.atomic_db.atomic_levels):
             renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]]/actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
+            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] /\
+                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
             for index in range(len(renate)-1):
-                if renate[index]!=0:
+                if renate[index] != 0:
                     self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
