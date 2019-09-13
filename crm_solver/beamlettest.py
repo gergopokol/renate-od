c88237247da9da1749440d7454152c4674ddea9b
asztalosors <asztalos@reak.bme.hu>
2019-09-13 13:02:12 +0200

    #87 Adding framework for unittesting.

---
+++ crm_solver/beamlettest.py
@@ -0,0 +1,10 @@
+from crm_solver.beamlet import Beamlet
+import unittest
+import numpy
+import scipy
+import utility.convert as uc
+import pandas
+
+
+class BeamletTest(unittest.TestCase):
+    pass
