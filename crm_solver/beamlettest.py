a821e601d30449b0d2878f830ccc6967bb5ce034
asztalosors <asztalos@reak.bme.hu>
2019-09-16 13:18:20 +0200

    #87 Adding tests for initial conditions.

--- crm_solver/beamlettest.py
+++ crm_solver/beamlettest.py
@@ -13,3 +13,10 @@ class BeamletTest(unittest.TestCase):
         actual = Beamlet()
         for attr in self.EXPECTED_ATTR:
             assert hasattr(actual, attr)
+
+    def test_initial_conditions(self):
+        actual = Beamlet()
+        self.assertIsInstance(actual.initial_condition, list, msg='Initial condition is of wrong type. '
+                                                                  'Expected type: list')
+        self.assertEqual(len(actual.initial_condition), actual.atomic_db.atomic_levels, msg='Initial conditions must '
+                         'match number of atomic levels.')
