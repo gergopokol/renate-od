f852f24d1bdce480a2f72463fb541e23212e454e
asztalosors <asztalos@reak.bme.hu>
2019-10-18 02:04:01 +0200

    #59 Adding setUp, tearDown and attribute existance tests.

--- crm_solver/coefficientmatrixtest.py
+++ crm_solver/coefficientmatrixtest.py
@@ -1,55 +1,52 @@
-import  unittest
+import unittest
 from crm_solver.atomic_db import AtomicDB
 from crm_solver.coefficientmatrix import CoefficientMatrix
 from utility.getdata import GetData
-from crm_solver.beamlet import Beamlet
 import numpy
-import  pandas
-from scipy.interpolate import interp1d
+import pandas
 
 
 class CoefficientMatrixTest(unittest.TestCase):
-    FROM_LEVEL_0 = 0
-    ATOMIC_DB = AtomicDB(data_path='dummy_data_path')
-    BEAMLET_PARAM = GetData(data_path_name="beamlet/dummy_param.xml").data
-    HDF5_PATH = BEAMLET_PARAM.getroot().find('body').find('beamlet_source').text
-    BEAMLET_PROFILES = GetData(data_path_name=HDF5_PATH, data_key=['profiles']).data
-    PLASMA_COMPONENTS = GetData(data_path_name=HDF5_PATH, data_key=['components']).data
 
-    def test_spontaneous_trans(self):
-        coeff_matrix = CoefficientMatrix(beamlet_param=self.BEAMLET_PARAM, beamlet_profiles=self.BEAMLET_PROFILES,
-                                         plasma_components=self.PLASMA_COMPONENTS, atomic_db=self.ATOMIC_DB)
-        self.assertEqual(coeff_matrix.spontaneous_trans_np.shape, self.ATOMIC_DB.spontaneous_trans.shape)
-        for i in range(self.ATOMIC_DB.atomic_levels):
-            for j in range(self.ATOMIC_DB.atomic_levels):
-                if i < j+1:
-                    self.assertEqual(coeff_matrix.spontaneous_trans_np[i, j], 0)
+    elements = ['1H+', '1D+', '3He+', '4He++', '9Be++++']
+    q = [1, 1, 1, 2, 4]
+    z = [1, 1, 2, 2, 4]
+    a = [1, 2, 3, 4, 9]
+    COMPONENTS = pandas.DataFrame([[-1] + q, [0] + z, [0] + a], index=['q', 'Z', 'A'],
+                                  columns=['electron'] + ['ion' + str(i + 1) for i in
+                                  range(len(elements))]).transpose()
 
-    def test_electron_loss(self):
-        coeff_matrix = CoefficientMatrix(beamlet_param=self.BEAMLET_PARAM,
-                                         beamlet_profiles=self.BEAMLET_PROFILES,
-                                         plasma_components=self.PLASMA_COMPONENTS, atomic_db=self.ATOMIC_DB)
-        self.assertEqual(coeff_matrix.electron_impact_trans_np.shape.size, 2)
-        self.assertEqual(coeff_matrix.electron_impact_loss_np.shape[0], self.ATOMIC_DB.electron_impact_loss.shape[1])
-        for i in range(self.ATOMIC_DB.atomic_levels):
-            for k in range(self.BEAMLET_PROFILES['beamlet_grid'].size):
-                self.assertEqual(coeff_matrix.electron_impact_loss_np[i, k], self.ATOMIC_DB.electron_impact_loss[i][0](
-                    self.BEAMLET_PROFILES['electron']['temperature']['eV'][k]))
+    grid = numpy.array([1., 2., 3., 4., 5., 6., 7.])
+    temperature = numpy.array([0, 1, 2, 2.5, 3, 8, 10])
+    density = numpy.array([1]*len(grid))
+    profile = numpy.stack([grid, density, temperature])
 
-    def test_electron_trans(self):
-        coeff_matrix = CoefficientMatrix(beamlet_param=self.BEAMLET_PARAM,
-                                         beamlet_profiles=self.BEAMLET_PROFILES,
-                                         plasma_components=self.PLASMA_COMPONENTS, atomic_db=self.ATOMIC_DB)
-        self.assertEqual(coeff_matrix.electron_impact_trans_np.shape.size, 3)
-        self.assertEqual(coeff_matrix.electron_impact_trans_np.shape, self.ATOMIC_DB.electron_impact_trans.shape[:2])
-        for i in range(self.ATOMIC_DB.atomic_levels):
-            for j in range(self.ATOMIC_DB.atomic_levels):
-                for k in range(self.BEAMLET_PROFILES['beamlet_grid'].size):
-                    self.assertEqual(coeff_matrix.electron_impact_trans_np[i, j, k], self.ATOMIC_DB.electron_impact_trans[i][j](
-                        self.BEAMLET_PROFILES['electron']['temperature']['eV'][:]))
+    comp = ['beamlet grid', 'electron', 'electron']
+    properties = ['distance', 'density', 'temperature']
+    units = ['m', 'm-3', 'eV']
 
+    for item in range(len(a)):
+        profile = numpy.vstack([profile, numpy.array([a[item]]*len(grid)), temperature])
+        comp = comp + ['ion' + str(item + 1)] * 2
+        properties = properties + ['density'] + ['temperature']
+        units = units + ['m-3'] + ['eV']
 
+    PROFILES = pandas.DataFrame(profile, pandas.MultiIndex.from_arrays([comp, properties, units],
+                                                                       names=['type', 'property', 'unit'])).transpose()
+    INPUT_DUMMY_PATH = 'beamlet/dummy0001.xml'
+    ATOMIC_DB = AtomicDB(data_path=INPUT_DUMMY_PATH, components=COMPONENTS)
+    BEAMLET_PARAM = GetData(data_path_name=INPUT_DUMMY_PATH).data
 
+    EXPECTED_ATTRIBUTES = ['matrix', 'electron_terms', 'ion_terms', 'photon_terms', 'beamlet_profiles',
+                           'electron_impact_trans_np', 'electron_impact_loss_np', 'ion_impact_trans_np',
+                           'ion_impact_loss_np']
 
+    def setUp(self):
+        self.RATE_MATRIX = CoefficientMatrix(self.BEAMLET_PARAM, self.PROFILES, self.COMPONENTS, self.ATOMIC_DB)
 
+    def tearDown(self):
+        pass
 
+    def test_all_attributes(self):
+        for attr in self.EXPECTED_ATTRIBUTES:
+            assert hasattr(self.RATE_MATRIX, attr)
