b27d1df63684b7bbceb4b8f4645bfa655041360a
asztalosors <asztalos@reak.bme.hu>
2019-10-31 12:27:19 +0100

    #75 Adding dedicatde test cases for RENATE-OD.

---
+++ crm_solver/crmtestcase.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+class CrmTestCase(unittest.TestCase):
+    def assertAlmostEqualRateEvolution(self, actual, reference, precision=1E-3, msg=''):
+        if reference.profiles['level '+actual.atomic_db.inv_atomic_dict[0]][0] == 1:
+            normalization_factor = actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]][0]
+        else:
+            normalization_factor = 1.
+        for level_index in range(actual.atomic_db.atomic_levels):
+
+
+    def assertNotAlmostEqualRateEvolution(self, actual, reference, precision=1E-3, msg=''):
+        pass
+
+    def assertAlmostEqualEmissionDensity(self, actual, reference, precision=1E-3, msg=''):
+        pass
+
+    def assertNotAlmostEqualEmissionDensity(self, actual, reference, precision=1E-3, msg=''):
+        pass
+
+    def assertAlmostEqualBeamAttenuation(self, actual, reference, precision=1E-3, msg=''):
+        pass
+
+    def assertNotAlmostEqualBeamAttenuation(self, actual, reference, precision=1E-3, msg=''):
+        pass
+
+    def assertAlmostEqualRelativePopulation(self, actual, reference, precision=1E-3, msg=''):
+        pass
+
+    def assertNotAlmostEqualRelativePopulation(self, actual, reference, precision=1E-3, msg=''):
+        pass
