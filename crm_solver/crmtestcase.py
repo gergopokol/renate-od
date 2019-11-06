9abce2f73d04df67b76070af8cf07047e25d6150
asztalosors <asztalos@reak.bme.hu>
2019-11-06 12:10:52 +0100

    #75 Fixed renate rate evolution benchmark.

--- crm_solver/crmtestcase.py
+++ crm_solver/crmtestcase.py
@@ -8,7 +8,11 @@ class CrmTestCase(unittest.TestCase):
         else:
             normalization_factor = 1.
         for level_index in range(actual.atomic_db.atomic_levels):
-
+            for index in range(len(actual.profiles)):
+                if actual.profiles['level '+actual.atomic_db.inv_atomic_dict[level_index]][index] != 0:
+                    self.assertTrue(abs(actual.profiles['level '+actual.atomic_db.inv_atomic_dict[level_index]][index] -
+                                        reference.profiles['level ' + actual.atomic_db.inv_atomic_dict[level_index]][index])/
+                                    reference.profiles['level ' + actual.atomic_db.inv_atomic_dict[level_index]][index] <= precision)
 
     def assertNotAlmostEqualRateEvolution(self, actual, reference, precision=1E-3, msg=''):
         pass
