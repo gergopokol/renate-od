21989bd7ca4b62190b52571b9b3c37a2af5d4e73
asztalosors <asztalos@reak.bme.hu>
2019-11-07 14:51:03 +0100

    #98 Adding corrections to the output routines.

--- utility/writedata.py
+++ utility/writedata.py
@@ -2,24 +2,24 @@ from utility.getdata import GetData
 from lxml import etree
 
 
-
 class WriteData:
     def __init__(self, root_path="data/"):
         self.root_path = root_path
 
-    def write_beamlet_profiles(self, param, profiles, subdir = 'output/beamlet/'):
-        output_path = param.getroot().find('head').find('id').text
+    def write_beamlet_profiles(self, beamlet, subdir='output/beamlet/'):
+        output_path = beamlet.param.getroot().find('head').find('id').text
         h5_output_path = subdir + output_path + ".h5"
-        xml_output_path = subdir +output_path + ".xml"
+        xml_output_path = subdir + output_path + ".xml"
         GetData.ensure_dir(self.root_path + h5_output_path)
         try:
-            profiles.to_hdf(path_or_buf=self.root_path + h5_output_path, key="profiles")
-            if not isinstance(param.getroot().find('body').find('beamlet_profiles'), etree._Element):
+            beamlet.profiles.to_hdf(path_or_buf=self.root_path + h5_output_path, key="profiles")
+            beamlet.components.to_hdf(path_or_buf=self.root_path + h5_output_path, key="components")
+            if not isinstance(beamlet.param.getroot().find('body').find('beamlet_profiles'), etree._Element):
                 new_element = etree.Element('beamlet_profiles')
                 new_element.text = h5_output_path
                 new_element.set('unit', '-')
-                param.getroot().find('body').append(new_element)
-            param.write(self.root_path + xml_output_path)
+                beamlet.param.getroot().find('body').append(new_element)
+            beamlet.param.write(self.root_path + xml_output_path)
             print('Beamlet profile data written to file: ' + subdir + output_path)
         except:
             print('Beamlet profile data could NOT be written to file: ' + subdir + output_path)
