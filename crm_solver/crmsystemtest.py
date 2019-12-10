6f379295b231242622a0f58ac808dd3141f058a2
asztalosors <asztalos@reak.bme.hu>
2019-12-10 17:18:01 +0100

    #75 Adding functioning regression tests.

--- crm_solver/crmsystemtest.py
+++ crm_solver/crmsystemtest.py
@@ -1,75 +1,23 @@
-import unittest
 from crm_solver.beamlet import Beamlet
 from crm_solver.crmtestcase import CrmTestCase
 from copy import deepcopy
+import os
 
 
 class CrmSystemTest(CrmTestCase):
-    path = 'test_dataset/crm_systemtests/renate_idl/scenario-standard_plasma-H_energy-100_beam-Na_profile.xml'
 
-    def test(self):
-        reference = Beamlet(data_path=self.path, solver='disregard')
-        actual_source = deepcopy(reference)
-        actual = Beamlet(param=actual_source.param, profiles=actual_source.profiles,
-                         components=actual_source.components, atomic_db=actual_source.atomic_db, solver='numerical')
-        self.assertAlmostEqualRateEvolution(actual, reference)
+    def setUp(self):
+        self.test_cases = ['H_test_case', 'D_test_case', 'T_test_case', 'Li_test_case', 'Na_test_case']
 
-
-class CrmAcceptanceTest(unittest.TestCase):
-
-    RELATIVE_PRECISION = 0.001
-    lithium_testcase = 'beamlet/acceptancetest/scenario-standard_plasma-H_energy-100_beam-Li_profile.xml'
-    sodium_testcase = 'beamlet/acceptancetest/scenario-standard_plasma-H_energy-100_beam-Na_profile.xml'
-    hydrogen_testcase = 'beamlet/acceptancetest/scenario-standard_plasma-H_energy-100_beam-H_profile.xml'
-    deuterium_testcase = 'beamlet/acceptancetest/scenario-standard_plasma-H_energy-100_beam-D_profile.xml'
-    tritium_testcase = 'beamlet/acceptancetest/scenario-standard_plasma-H_energy-100_beam-T_profile.xml'
-
-    def test_lithium_beamlet(self):
-        actual = Beamlet(data_path=self.lithium_testcase)
-        for level in range(actual.atomic_db.atomic_levels):
-            renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] / \
-                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
-            for index in range(len(renate)-1):
-                if renate[index] != 0:
-                    self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
-
-    def test_sodium_beamlet(self):
-        actual = Beamlet(data_path=self.sodium_testcase)
-        for level in range(actual.atomic_db.atomic_levels):
-            renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] / \
-                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
-            for index in range(len(renate)-1):
-                if renate[index] != 0:
-                    self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
-
-    def test_hydrogen_beamlet(self):
-        actual = Beamlet(data_path=self.hydrogen_testcase)
-        for level in range(actual.atomic_db.atomic_levels):
-            renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] /\
-                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
-            for index in range(len(renate)-1):
-                if renate[index] != 0:
-                    self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
-
-    def test_deuterium_beamlet(self):
-        actual = Beamlet(data_path=self.deuterium_testcase)
-        for level in range(actual.atomic_db.atomic_levels):
-            renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] / \
-                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
-            for index in range(len(renate)-1):
-                if renate[index] != 0:
-                    self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
-
-    def test_tritium_beamlet(self):
-        actual = Beamlet(data_path=self.tritium_testcase)
-        for level in range(actual.atomic_db.atomic_levels):
-            renate = actual.profiles['RENATE level ' + str(level)]
-            renate_od = actual.profiles['level ' + actual.atomic_db.inv_atomic_dict[level]] /\
-                actual.profiles['level '+actual.atomic_db.inv_atomic_dict[0]].max()
-            for index in range(len(renate)-1):
-                if renate[index] != 0:
-                    self.assertTrue(abs(renate[index] - renate_od[index])/renate[index] <= self.RELATIVE_PRECISION)
+    def test_crm_regression(self):
+        for test_case in self.test_cases:
+            path = 'test_dataset/crm_systemtests/actual/'+test_case+'.xml'
+            reference = Beamlet(data_path=path, solver='disregard')
+            actual_source = deepcopy(reference)
+            actual = Beamlet(param=actual_source.param, profiles=actual_source.profiles,
+                             components=actual_source.components, atomic_db=actual_source.atomic_db, solver='numerical')
+            msg = 'Failure for following test case: '+test_case+'\n'
+            self.assertAlmostEqualRateEvolution(actual, reference, msg=msg)
+            self.assertAlmostEqualBeamAttenuation(actual, reference, msg=msg)
+            self.assertAlmostEqualEmissionDensity(actual, reference, msg=msg)
+            self.assertAlmostEqualRelativePopulation(actual, reference, msg=msg)
