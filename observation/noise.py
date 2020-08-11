f4fc8dfd69f90708f3cc50e35a45db9532453594
asztalosors <asztalos@reak.bme.hu>
2020-08-11 17:32:44 +0200

    #112 Added some basic sceleton of loading sequence.

--- observation/noise.py
+++ observation/noise.py
@@ -3,7 +3,13 @@ import scipy
 
 
 class RenateSyntheticDiagnostic:
-    pass
+    def __init__(self, synthetic_signals=None, data_path='siganls/test.txt'):
+        self.signals = synthetic_signals
+        if self.signals is None:
+            self._load_synthetic_signals()
+
+    def _load_synthetic_signals(self):
+        pass
 
 
 class Noise:
