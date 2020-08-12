e88248f9c5dd73ded159d8feeb9862fdc5df4ea5
asztalosors <asztalos@reak.bme.hu>
2020-08-12 13:09:13 +0200

    #112 Added signals data format checks.

--- observation/noise.py
+++ observation/noise.py
@@ -1,6 +1,7 @@
 import numpy
 import scipy
 import utility.getdata as ut
+import pandas
 
 
 class SynthSignals:
@@ -9,9 +10,17 @@ class SynthSignals:
             self.frequency = frequency
         else:
             raise TypeError('Expected data type for <frequency> is <int>.')
-        self._check_synthetic_signals(synthetic_signals)
+        self._check_synthetic_signals(synthetic_signals, data_path)
 
-    def _check_synthetic_signals(self, synthetic_signals):
+    def _check_synthetic_signals(self, synthetic_signals, path):
+        if synthetic_signals is None:
+            self._load_synthetic_signals(path)
+        elif isinstance(synthetic_signals, pandas.DataFrame) or isinstance(synthetic_signals, pandas.Series):
+            self.signals = synthetic_signals
+        else:
+            raise TypeError('The expected data type for <synthetic signals> is <pandas.DataFrame or pandas.Series>')
+
+    def _load_synthetic_signals(self, path):
         pass
 
 
