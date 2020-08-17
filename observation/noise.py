fca0f9a6debf4555946eb76c6f10e3fa79b7ad49
andorfi <andorfi.istvan@reak.bme.hu>
2020-08-17 15:58:42 +0200

    #97 Started work at making the noise modelling. Defined the fundamental classes. Generator class is able to create Poisson or Gaussian random numbers, Parameters class store the parameters of te circuit, Constants class stores the physical constants, and Noise class is able to create different types of noise.

--- observation/noise.py
+++ observation/noise.py
@@ -24,6 +24,67 @@ class SynthSignals:
         self.signals = ut.GetData(data_path_name=path, data_format="array").data
 
 
-class Noise:
+class Generator:
+    def __init__(self, DispersionType, Mean, Deviation):
+        self.DispersionType = DispersionType
+        self.Mean = Mean
+        self.Deviation = Deviation
+
+    def Generate(self):
+        if self.DispersionType == "Poisson":
+            PoissonRandNumb = numpy.random.poisson(self.Mean)
+            return PoissonRandNumb
+
+        if self.DispersionType == "Gaussian":
+            GaussianRandNumb = numpy.random.normal(loc=self.Mean, scale=self.Deviation)
+            return GaussianRandNumb
+
     pass
 
+class Parameters:
+    def __init__(self, Gain, QuantumEfficiency, NoiseIndex, Bandwidth, LoadResistance, LoadCapacity, DarkCurrent,
+                 Temperature, VoltageNoise, InternalCapacities):
+        self.Gain = Gain
+        self.QuantumEfficiency = QuantumEfficiency
+        self.NoiseIndex = NoiseIndex
+        self.Bandwidth = Bandwidth
+        self.LoadResistance = LoadResistance
+        self.LoadCapacity = LoadCapacity
+        self.DarkCurrent = DarkCurrent
+        self.Temperature = Temperature
+        self.VoltageNoise = VoltageNoise
+        self.InternalCapacities = InternalCapacities
+    pass
+
+class Constants:
+    def __init__(self, Boltzmanns=1.380649e-23, Electroncharge=1.60217662e-19):
+        self.Boltzmanns = Boltzmanns
+        self.Electroncharge = Electroncharge
+    pass
+
+class Noise(Generator, Parameters, Constants):
+    def __init__(self, NoiseType):
+        self.NoiseType = NoiseType
+
+    def NoiseGeneration(self):
+        p = Parameters()
+        c = Constants()
+        if self.NoiseType == "Johnson":
+            Mean = 0
+            Deviation = scipy.sqrt(4 * c.Boltzmanns * p.Temperature * p.Bandwidth * p.LoadResistance)
+            g = Generator("Gaussian", Mean, Deviation)
+            Johnsonnoise = g.Generate()
+            return Johnsonnoise
+        if self.NoiseType == "Voltage":
+            Mean = 0
+            Deviation = p.VoltageNoise * scipy.sqrt(1 / (2 * scipy.pi * p.LoadResistance * (p.LoadCapacity + p.InternalCapacities))) + p.VoltageNoise * (
+                        1 + p.InternalCapacities / p.LoadCapacity) * scipy.sqrt(1.57 * 1 / (2 * scipy.pi * p.LoadResistance* p.LoadCapacity))
+            g = Generator("Gaussian", Mean, Deviation)
+            Voltagenoise = g.Generate()
+            return Voltagenoise
+        if self.NoiseType == "Dark":
+            Mean = 0
+            Deviation = scipy.sqrt(2 * c.Electroncharge * p.DarkCurrent * p.Bandwidth) * p.LoadResistance
+            g = Generator("Gaussian", Mean, Deviation)
+            Darknoise = g.Generate()
+            return Darknoise
