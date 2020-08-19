000ac71fd6d25865191323c514b100c779ccb414
andorfi <andorfi.istvan@reak.bme.hu>
2020-08-19 12:52:16 +0200

    #97 Change the capital at Boltzmann's constant's name

--- utility/constants.py
+++ utility/constants.py
@@ -2,4 +2,4 @@ class Constants:
     def __init__(self):
         self.charge_electron = 1.602176487e-19   #[C]
         self.speed_of_light = 299792458       #[m / s]
-        self.Boltzmanns_constant = 1.380649e-23   #[J/K]
+        self.boltzmanns_constant = 1.380649e-23   #[J/K]
