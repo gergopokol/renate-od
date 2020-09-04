557057e1b8d08536bfd7aefe314f68492a441bfc
asztalosors <asztalos@reak.bme.hu>
2020-09-04 13:05:27 +0200

    #119 Removed Generator related testing frame and code from Noise unittesting.

--- observation/noisetest.py
+++ observation/noisetest.py
@@ -1,16 +1,15 @@
 import unittest
 import numpy
 import pandas
-from observation.noise import Generator
 
 
-class GeneratorTest(unittest.TestCase):
+class NoiseTest(unittest.TestCase):
 
     INPUT_SIGNAL = pandas.Series(data=numpy.full(100, 1E9))
 
     def setUp(self):
-        self.generator = Generator(Data=self.INPUT_SIGNAL)
+        pass
 
     def tearDown(self):
-        del self.generator
+        pass
 
