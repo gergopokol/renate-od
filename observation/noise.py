9066e7344c8bf7dc4004ea54e5478248a7c9424a
asztalosors <asztalos@reak.bme.hu>
2020-09-05 19:12:30 +0200

    #123 Adding shell for PP detector description.

--- observation/noise.py
+++ observation/noise.py
@@ -86,6 +86,12 @@ class PMT(Noise):
 
 
 class PP(Noise):
+    def __init__(self, detector_parameters):
+        Noise.__init__(self)
+        self.__setup_detector_parameters(detector_parameters)
+
+    def __setup_detector_parameters(self, detector_parameters):
+        pass
 
     def add_noise_to_signal(self):
         pass
