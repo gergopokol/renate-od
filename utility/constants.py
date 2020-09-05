85911ecf5590d437718af964c37f6f1a4a0342c4
asztalosors <asztalos@reak.bme.hu>
2020-09-05 20:09:26 +0200

    #110 Making changes to constant class to be set up enviroment dependantly.

--- utility/constants.py
+++ utility/constants.py
@@ -1,5 +1,17 @@
+import scipy.constants as c
+
+
 class Constants:
     def __init__(self):
-        self.charge_electron = 1.602176487e-19   #[C]
-        self.speed_of_light = 299792458       #[m / s]
-        self.boltzmanns_constant = 1.380649e-23   #[J/K]
+        try:
+            import imas
+        except ImportError:
+            self.__setup_standalone_constants_db()
+
+    def __setup_standalone_constants_db(self):
+        self.charge_electron = c.elementary_charge
+        self.speed_of_light = c.speed_of_light
+        self.Boltzmann = c.Boltzmann
+
+    def __setup_imas_constants_db(self):
+        pass
