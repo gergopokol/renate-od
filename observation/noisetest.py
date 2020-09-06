8d10d6717cc73f23e9ee5e51d4f28cc899663750
asztalosors <asztalos@reak.bme.hu>
2020-09-06 11:47:39 +0200

    #125 Adding shell for gaussian and poisson as well as seeded generator tests.

--- observation/noisetest.py
+++ observation/noisetest.py
@@ -1,15 +1,24 @@
 import unittest
 import numpy
-import pandas
+from observation.noise import Noise, APD, PMT, PP, Detector
 
 
 class NoiseTest(unittest.TestCase):
 
-    INPUT_SIGNAL = pandas.Series(data=numpy.full(100, 1E9))
+    INPUT_SIGNAL_CONSTANT = numpy.full(100, 10000)
+    INSTANCE_NR = 100000
 
     def setUp(self):
-        pass
+        self.noise_gen = Noise()
 
     def tearDown(self):
+        del self.noise_gen
+
+    def test_poisson_generator(self):
         pass
 
+    def test_gaussian_generator(self):
+        pass
+
+    def test_seeded_generator(self):
+        pass
