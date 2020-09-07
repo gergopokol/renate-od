32cbc50c6c329f29771b0b5c2f81fdb00727fce4
asztalosors <asztalos@reak.bme.hu>
2020-09-07 16:55:25 +0200

    #125 Creating a general noise generator test routines and framework. To be implemneted later.

--- observation/noisetest.py
+++ observation/noisetest.py
@@ -1,10 +1,40 @@
 import unittest
 import numpy
+import scipy.stats as st
+from unittest.util import safe_repr
 from observation.noise import Noise, APD, PMT, PP, Detector
 
 
 class NoiseBasicTestCase(unittest.TestCase):
-    pass
+    @staticmethod
+    def _WithinPrecision(actual, reference, precision):
+        if abs(actual - reference)/reference <= precision:
+            return True, 'Actual precision: \t %s, Reference precision: \t %s' % (safe_repr(abs(actual - reference)
+                                                                                            / reference), precision)
+        else:
+            return False, 'Actual precision: \t %s, Reference precision: \t %s' % (safe_repr(abs(actual - reference)
+                                                                                             / reference), precision)
+
+    def assertDistributionVariance(self, series, reference, precision=1E-2, msg=''):
+        actual = series.var()
+        status, statement = self._WithinPrecision(actual, reference, precision)
+        if not status:
+            standardMsg = ' \n Actual distribution function variance: %s and reference variance: %s are not within ' \
+                          'precision margin. \n' % (safe_repr(actual), safe_repr(reference)) + statement
+            msg = self._formatMessage(msg, standardMsg)
+            self.fail(msg)
+
+    def assertDistributionMean(self):
+        pass
+
+    def assertDistributionStandardDeviation(self):
+        pass
+
+    def assertDistributionSkewness(self):
+        pass
+
+    def assertDistributionKurtosis(self):
+        pass
 
 
 class NoiseGeneratorTest(NoiseBasicTestCase):
@@ -19,10 +49,11 @@ class NoiseGeneratorTest(NoiseBasicTestCase):
         del self.noise_gen
 
     def test_poisson_generator(self):
-        pass
+        test_data = self.noise_gen.poisson(numpy.full(self.INPUT_INSTANCE, self.INPUT_VALUE))
+        self.assertDistributionVariance(test_data, self.INPUT_VALUE, msg='Poisson generator variance test FAIL.')
 
     def test_gaussian_generator(self):
-        pass
+        passgit 
 
     def test_seeded_generator(self):
         pass
