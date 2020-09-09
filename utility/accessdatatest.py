ff9535169e98889d743270fdce65e43b02379e14
asztalosors <asztalos@reak.bme.hu>
2020-09-09 23:08:00 +0200

    #130 Added Unittest for AccessData class and shell.

---
+++ utility/accessdatatest.py
@@ -0,0 +1,11 @@
+import unittest
+from utility.accessdata import AccessData
+
+
+class AccessDataTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
