d246b02d1f785b763055feb28bc973c400b33470
asztalosors <asztalos@reak.bme.hu>
2020-09-10 20:12:16 +0200

    #130 Adding tests for private and public server addresses.

--- utility/accessdatatest.py
+++ utility/accessdatatest.py
@@ -7,9 +7,21 @@ class AccessDataTest(unittest.TestCase):
     TEST_PATH = 'test_dataset/access_tests/'
     PUBLIC_TEST = 'public_test'
     PRIVATE_TEST = 'private_test'
+    PRIVATE_SERVES_ADDRESS = 'data@deep.reak.bme.hu:~/private_html/renate-od'
+    PUBLIC_SERVER_ADDRESS = 'http://deep.reak.bme.hu/~data/renate-od'
 
     def setUp(self):
         self.access = AccessData(None)
 
     def tearDown(self):
         del self.access
+
+    def test_private_data_path(self):
+        self.assertEqual(self.access.server_private_address, self.PRIVATE_SERVES_ADDRESS,
+                         msg='Server private data address does not match.')
+
+    def test_public_server_address(self):
+        self.assertEqual(self.access.server_public_address, self.PUBLIC_SERVER_ADDRESS,
+                         msg='Server public data address does not match.')
+
+
