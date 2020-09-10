17f47a36ab5246c3f7ffed314974398475073dc0
asztalosors <asztalos@reak.bme.hu>
2020-09-10 16:50:26 +0200

    #130 Adding setUp and tearDown routines and some paths.

--- utility/accessdatatest.py
+++ utility/accessdatatest.py
@@ -4,8 +4,12 @@ from utility.accessdata import AccessData
 
 class AccessDataTest(unittest.TestCase):
 
+    TEST_PATH = 'test_dataset/access_tests/'
+    PUBLIC_TEST = 'public_test'
+    PRIVATE_TEST = 'private_test'
+
     def setUp(self):
-        pass
+        self.access = AccessData(None)
 
     def tearDown(self):
-        pass
+        del self.access
