547f33f3d487e184174ca5a931a88f4e400ae520
asztalosors <asztalos@reak.bme.hu>
2020-10-01 16:02:02 +0200

    #141 Creating info loading routine.

--- utility/manage.py
+++ utility/manage.py
@@ -1,3 +1,5 @@
+import os
+from lxml import etree
 from crm_solver.beamlet import Beamlet
 from utility.writedata import WriteData
 from utility.putdata import PutData
@@ -6,7 +8,22 @@ from utility.getdata import GetData
 
 class CodeInfo(object):
     def __init__(self):
-        pass
+        self.code_info_path = os.path.join(os.path.dirname(__file__), '..', 'RENATE-OD.info')
+        self._load_code_info()
+
+    def _load_code_info(self):
+        code_info_tree = etree.parse(self.code_info_path)
+        self.code_name = code_info_tree.find('head').find('name').text
+        self.code_abbr = code_info_tree.find('head').find('abbreviation').text
+        self.code_license = code_info_tree.find('body').find('license').text
+        self.code_version = code_info_tree.find('body').find('version').text
+        self.code_install_requirements = code_info_tree.find('body').find('install_requirements').text
+        self.code_package = code_info_tree.find('body').find('package_name').text
+        self.code_git_link = code_info_tree.find('body').find('git').text
+        self.classifier_audience = code_info_tree.find('classifiers').find('audience').text
+        self.classifier_natural_language = code_info_tree.find('classifiers').find('natural_language').text
+        self.classifier_programming_language = code_info_tree.find('classifiers').find('programming_language').text
+        self.classifier_topic = code_info_tree.find('classifiers').find('topic').text
 
     def update_data(self, attribute, value):
         pass
