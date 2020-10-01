2ea91d58a0cb5f83ce17f6914fe91f0277f9ae12
asztalosors <asztalos@reak.bme.hu>
2020-10-01 16:08:47 +0200

    #141 Checking whether user is authorised or not.

--- utility/manage.py
+++ utility/manage.py
@@ -4,12 +4,14 @@ from crm_solver.beamlet import Beamlet
 from utility.writedata import WriteData
 from utility.putdata import PutData
 from utility.getdata import GetData
+from utility.accessdata import AccessData
 
 
 class CodeInfo(object):
     def __init__(self):
         self.code_info_path = os.path.join(os.path.dirname(__file__), '..', 'RENATE-OD.info')
         self._load_code_info()
+        self._check_authorised_user()
 
     def _load_code_info(self):
         code_info_tree = etree.parse(self.code_info_path)
@@ -24,6 +26,13 @@ class CodeInfo(object):
         self.classifier_natural_language = code_info_tree.find('classifiers').find('natural_language').text
         self.classifier_programming_language = code_info_tree.find('classifiers').find('programming_language').text
         self.classifier_topic = code_info_tree.find('classifiers').find('topic').text
+
+    def _check_authorised_user(self):
+        access = AccessData(data_path_name=None)
+        if access.private_key is None:
+            self.authorization = False
+        else:
+            self.authorization = True
 
     def update_data(self, attribute, value):
         pass
