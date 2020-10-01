b62789f016e26f72d1b62a1a1fd810d4302c5fca
asztalosors <asztalos@reak.bme.hu>
2020-10-01 13:20:42 +0200

    #141 Adding manage.py and created shell for CodeParameters and Release classes.

---
+++ manage.py
@@ -0,0 +1,17 @@
+from crm_solver.beamlet import Beamlet
+from utility.writedata import WriteData
+from utility.putdata import PutData
+from utility.getdata import GetData
+
+
+class CodeParameters(object):
+    def __init__(self):
+        pass
+
+    def update_data(self, attribute, value):
+        pass
+
+
+class Release(object):
+    def __init__(self):
+        pass
