5b9982d6c8e3cf8278565aef66953b47b4a42069
asztalosors <asztalos@reak.bme.hu>
2020-10-05 14:34:25 +0200

    #143 Adding major, minor and bugfix release tests.

--- utility/managetest.py
+++ utility/managetest.py
@@ -11,9 +11,9 @@ class VersionTest(TestCase):
     GREATER_THEN_TEST_FALSE = ['3.4.5', '4.4.5', '3.5.4', '3.4.6']
     LESS_THEN_TEST_TRUE = ['4.4.5', '3.5.4', '3.4.6']
     LESS_THEN_TEST_FALSE = ['3.4.5', '2.10.3', '3.3.5', '3.4.4']
-    MAJOR_RELEASE_TEST = '4.0.0'
-    MINOR_RELEASE_TEST = '3.5.0'
-    BUGFIX_RELEASE_TEST = '3.4.6'
+    MAJOR_RELEASE_TEST = [4, 0, 0]
+    MINOR_RELEASE_TEST = [3, 5, 0]
+    BUGFIX_RELEASE_TEST = [3, 4, 6]
 
     def setUp(self):
         self.version = Version('3.4.5')
@@ -54,3 +54,36 @@ class VersionTest(TestCase):
             reference = Version(case)
             self.assertFalse(self.version < reference, msg='Object: %s is expected not to be less then %s' %
                                                            (safe_repr(self.version), safe_repr(reference)))
+
+    def test_major_release(self):
+        self.version.release_major_version()
+        expected_reference = str(self.MAJOR_RELEASE_TEST[0]) + '.' + \
+                             str(self.MAJOR_RELEASE_TEST[1]) + '.' + \
+                             str(self.MAJOR_RELEASE_TEST[2])
+        self.assertTrue(self.version.major_version == self.MAJOR_RELEASE_TEST[0] and
+                        self.version.minor_version == self.MAJOR_RELEASE_TEST[1] and
+                        self.version.bugfix_version == self.MAJOR_RELEASE_TEST[2],
+                        msg='Major release of object %s is expected to be %s.' %
+                            (safe_repr(self.version), safe_repr(expected_reference)))
+
+    def test_minor_release(self):
+        self.version.release_minor_version()
+        expected_reference = str(self.MINOR_RELEASE_TEST[0]) + '.' + \
+                             str(self.MINOR_RELEASE_TEST[1]) + '.' + \
+                             str(self.MINOR_RELEASE_TEST[2])
+        self.assertTrue(self.version.major_version == self.MINOR_RELEASE_TEST[0] and
+                        self.version.minor_version == self.MINOR_RELEASE_TEST[1] and
+                        self.version.bugfix_version == self.MINOR_RELEASE_TEST[2],
+                        msg='Major release of object %s is expected to be %s.' %
+                            (safe_repr(self.version), safe_repr(expected_reference)))
+
+    def test_bugfix_release(self):
+        self.version.release_bugfix_version()
+        expected_reference = str(self.BUGFIX_RELEASE_TEST[0]) + '.' + \
+                             str(self.BUGFIX_RELEASE_TEST[1]) + '.' + \
+                             str(self.BUGFIX_RELEASE_TEST[2])
+        self.assertTrue(self.version.major_version == self.BUGFIX_RELEASE_TEST[0] and
+                        self.version.minor_version == self.BUGFIX_RELEASE_TEST[1] and
+                        self.version.bugfix_version == self.BUGFIX_RELEASE_TEST[2],
+                        msg='Major release of object %s is expected to be %s.' %
+                            (safe_repr(self.version), safe_repr(expected_reference)))
