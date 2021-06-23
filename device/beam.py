a330c23a2ad0ce72d7a539d904e623c3b2166de7
Asztalos Örs <asztalos@reak.bme.hu>
2021-06-24 00:58:00 +0200

    #164 Adding sceleton for beam object.

---
+++ device/beam.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+
+class Beam(object):
+    def __init__(self):
+        pass
+
+    def get_beam_data(self):
+        pass
+
+    def generate_beamlets(self):
+        pass
