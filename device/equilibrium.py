2497a4a35abcca8b44206b6c1682c631a8abb0d4
Asztalos Örs <asztalos@reak.bme.hu>
2021-06-25 14:48:46 +0200

    #165 Adding hidden routines.

--- device/equilibrium.py
+++ device/equilibrium.py
@@ -12,23 +12,23 @@ class Equilibrium(object):
 
     def load_poloidal_flux_map(self):
         if self.data_source == 'renate':
-            self.load_renate_IDL_flux_map_data()
+            self.__load_renate_IDL_flux_map_data()
         elif self.data_source == 'ids':
-            self.load_ids_flux_map_data()
+            self.__load_ids_flux_map_data()
         else:
             raise ValueError('The data source requested: ' + str(self.data_source) + ' is not supported. ')
-        self.generate_point_to_flux_interpolator()
+        self.__generate_point_to_flux_interpolator()
 
-    def load_renate_IDL_flux_map_data(self):
+    def __load_renate_IDL_flux_map_data(self):
         idl_flux_map_data = GetData(data_path_name=self.data_path).data
         self.R = idl_flux_map_data['psi_rz_matrix'][0][1]
         self.Z = idl_flux_map_data['psi_rz_matrix'][0][2]
         self.Psi = idl_flux_map_data['psi_rz_matrix'][0][0]
 
-    def generate_point_to_flux_interpolator(self):
+    def __generate_point_to_flux_interpolator(self):
         self.point2flux = interp2d(self.R, self.Z, self.Psi)
 
-    def load_ids_flux_map_data(self):
+    def __load_ids_flux_map_data(self):
         pass
 
     def show_rzflux_map(self, levels=40, lcfs=False):
