87190da6090bf5cc4213dd9dd3106bf495316178
Asztalos Örs <asztalos@reak.bme.hu>
2021-06-25 13:35:25 +0200

    #165 Adding visualisation option.

--- device/equilibrium.py
+++ device/equilibrium.py
@@ -1,10 +1,11 @@
 import numpy as np
 from scipy.interpolate import interp2d
 from utility.getdata import GetData
+import matplotlib.pyplot as plt
 
 
 class Equilibrium(object):
-    def __init__(self, source=None, data_id=None, data_path=None):
+    def __init__(self, source='renate', data_id=None, data_path='device_data/test/test_idl_data.sav'):
         self.data_source = source
         self.data_path = data_path
         self.data_id = data_id
@@ -16,7 +17,6 @@ class Equilibrium(object):
             self.load_ids_flux_map_data()
         else:
             raise ValueError('The data source requested: ' + str(self.data_source) + ' is not supported. ')
-
         self.generate_point_to_flux_interpolator()
 
     def load_renate_IDL_flux_map_data(self):
@@ -30,3 +30,10 @@ class Equilibrium(object):
 
     def load_ids_flux_map_data(self):
         pass
+
+    def show_rzflux_map(self, levels=40, lcfs=False):
+        plt.contour(self.R, self.Z, self.Psi, levels)
+        if lcfs:
+            plt.contour(self.R, self.Z, self.Psi, [1], colors='r')
+        plt.axis('equal')
+        plt.show()
