576f69d143be79ac52da83d747541822e1b93229
Asztalos Örs <asztalos@reak.bme.hu>
2021-06-28 11:09:15 +0200

    #164 Adding basic stuff for beam object.

--- device/beam.py
+++ device/beam.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 
 class Beam(object):
-    def __init__(self):
-        pass
+    def __init__(self, beam_profile=None, source=None, data_path='device_data/test/test_beam.xml'):
+        self.beam_profile = beam_profile
+        self.beam_source = source
+        self.data_path = data_path
 
     def get_beam_data(self):
         pass
