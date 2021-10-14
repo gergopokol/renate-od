ce250a4dbcd6e7b020d6ce349f6942ae1728bb7b
asztalosors <asztalos@reak.bme.hu>
2021-10-14 14:43:34 +0200

    #182 Corrections to the trabnsition class.

--- atomic/tools.py
+++ atomic/tools.py
@@ -100,16 +100,23 @@ class IonizedMolecule(Molecule):
 
 
 class Transition(object):
-    def __init__(self, projectile=Particle, target=Particle, from_level=str, to_level=str, trans=str):
+    def __init__(self, projectile=Particle, target=Particle, from_level=str, to_level=None, trans=str):
         self.projectile = projectile
         self.target = target
-        if isinstance(from_level, str) and isinstance(to_level, str) and isinstance(trans, str):
+        if isinstance(from_level, str) and isinstance(trans, str):
             self.from_level = from_level
-            self.to_level = to_level
             if trans in ['ex', 'de-ex', 'eloss', 'ion', 'cx']:
                 self.transition = trans
             else:
                 InputError('The provided transition is not supported. '
                            'Supported transitions are: ex, de-ex, eloss, ion and cx.')
+            if (to_level is None) or isinstance(to_level, str):
+                self.to_level = to_level
+            else:
+                InputError('The provided end state for the electron transition is not valid. Str or None is expected.')
         else:
             InputError('Expected input data format for <from_level>, <to_level> and <trans> to be of str type.')
+
+    def __repr__(self):
+        return 'Collision of: '+str(self.projectile)+' + '+str(self.target)+' with transition: ' + \
+               self.transition+' | from level: '+self.from_level+' | to_level: '+str(self.to_level)
