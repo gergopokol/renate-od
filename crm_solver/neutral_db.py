1f896a53a37228665d159ab93df309268572d4d9
asztalosors <asztalos@reak.bme.hu>
2021-11-08 13:49:47 +0100

    #190 Added first version of neutral DB.

--- crm_solver/neutral_db.py
+++ crm_solver/neutral_db.py
@@ -1,13 +1,29 @@
-import numpy as np
 from utility.getdata import GetData
 
 
 class NeutralDB(object):
-    def __init__(self, path=None, param=None, components=None, resolved='bundled-n'):
+    def __init__(self, path=None, param=None, components=None, resolved='bundled_n'):
         self.param = param
         self.resolved = resolved
-        if (components['q'] == 0).any():
-            self.__load_neutral_cross_section()
+        self.__load_neutral_cross_section()
+        self.__get_atomic_levels()
+
+    def __get_atomic_levels(self):
+        self.atomic_levels = self.neutral_cross_sections.shape[0]
 
     def __load_neutral_cross_section(self):
-        pass
+        path = self.__set_neutral_cross_section_path()
+        self.neutral_cross_sections = GetData(data_path_name=path, data_format="array").data
+
+    def __set_neutral_cross_section_path(self, target='H'):
+        cross_section_path = 'atomic_data/' + self.param.getroot().find('body').find('beamlet_species').text + \
+                             'cross_sections/neutral/'
+        file_name = target + '_' + self.resolved + '_' + \
+                    self.param.getroot().find('body').find('beamlet_energy').text + '.txt'
+        return cross_section_path + file_name
+
+    def get_neutral_impact_loss(self, from_level):
+        return self.neutral_cross_sections[from_level, from_level]
+
+    def get_neutral_impact_transition(self, from_level, to_level):
+        return self.neutral_cross_sections[from_level, to_level]
