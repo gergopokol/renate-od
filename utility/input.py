89cb5fb30115f1eddbae4916f0c3d90368e96f10
asztalosors <asztalos@reak.bme.hu>
2021-11-11 16:16:00 +0100

    #200 Adding basics of input denerator class.

---
+++ utility/input.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from lxml import etree
+import numpy as np
+
+
+class BeamletInput(object):
+    def __init__(self, energy, projectile, source):
+        self.energy = energy
+        self.projectile = projectile
+        self.source = source
+
+    def add_grid(self, grid):
+        self.grid = np.array(grid)
+
+    def add_target(self, charge, atomic_number, mass_number, molecular_number, density, temperature):
+        pass
+
+    def make_input(self):
+        pass
+
+    def from_text(self, source, charges, atomic_numbers, mass_numbers, molecular_numbers):
+        pass
