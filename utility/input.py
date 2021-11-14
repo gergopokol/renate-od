cd4d1ba61dab103bb87bbce7171eeafc4f805ffb
asztalosors <asztalos@reak.bme.hu>
2021-11-14 23:23:31 +0100

    #200 Adding input generator for atomic db init. The class generates the param, xml and the components pandas dataframe and return them to init the Atomic DB.

--- utility/input.py
+++ utility/input.py
@@ -3,17 +3,76 @@ from lxml import etree
 import numpy as np
 
 
-class BeamletInput(object):
-    def __init__(self, energy, projectile, source):
+class AtomicInput(object):
+    def __init__(self, energy, projectile, source, current, param_name):
         self.energy = energy
         self.projectile = projectile
-        self.source = source
+        self.data_source = source
+        self.current = current
+        self.param_name = param_name
+        self.components = None
+        self.param = None
+        self.component_col = ['q', 'Z', 'A', 'Molecule']
+        self.component_index = []
+        self.component_data = []
+        self._build_param()
 
     def _build_param(self):
-        pass
+        xml = etree.Element('xml')
+        head = etree.SubElement(xml, 'head')
+        id_tag = etree.SubElement(head, 'id')
+        id_tag.text = self.param_name
+        body_tag = etree.SubElement(xml, 'body')
+        beamlet_energy = etree.SubElement(body_tag, 'beamlet_energy', {'unit': 'keV'})
+        beamlet_energy.text = str(self.energy)
+        beamlet_species = etree.SubElement(body_tag, 'beamlet_species')
+        beamlet_species.text = self.projectile
+        beamlet_source = etree.SubElement(body_tag, 'beamlet_source')
+        beamlet_source.text = self.data_source
+        beamlet_current = etree.SubElement(body_tag, 'beamlet_current', {'unit': 'A'})
+        beamlet_current.text = str(self.current)
+        self.param = etree.ElementTree(element=xml)
 
     def _build_components(self):
-        pass
+        if len(self.component_data) == 0:
+            raise ValueError('The component data base was not initiated.')
+        else:
+            self.components = pd.DataFrame(np.array(self.component_data), columns=self.component_col,
+                                           index=self.component_index)
+
+    def _next_element(self, name):
+        return name + str(len([component for component in self.component_index if name in component])+1)
+
+    def add_target_species(self, charge, atomic_number, mass_number, molecule_name):
+        element = [charge, atomic_number, mass_number, molecule_name]
+        if charge == -1:
+            if 'electron' in self.component_index:
+                print('Already a component.')
+            else:
+                self.component_index.append('electron')
+                self.component_data.append(element)
+        elif charge == 0:
+            if element in self.component_data:
+                print('Already a component.')
+            else:
+                self.component_index.append(self._next_element('neutral'))
+                self.component_data.append(element)
+        elif charge >= 1:
+            if element in self.component_data:
+                print('Already a component.')
+            else:
+                self.component_index.append(self._next_element('ion'))
+                self.component_data.append(element)
+
+    def get_atomic_db_input(self):
+        self._build_components()
+        return self.param, self.components
+
+
+class BeamletInput(AtomicInput):
+    def __init__(self, energy, projectile, source, current, param_name):
+        AtomicInput.__init__(self, energy=energy, projectile=projectile, source=source,
+                             current=current, param_name=param_name)
 
     def _build_profiles(self):
         pass
@@ -21,10 +80,10 @@ class BeamletInput(object):
     def add_grid(self, grid):
         self.grid = np.array(grid)
 
-    def add_target(self, charge, atomic_number, mass_number, molecular_number, density, temperature):
+    def add_target_profiles(self, charge, atomic_number, mass_number, molecular_number, density, temperature):
         pass
 
-    def make_input(self):
+    def get_beamlet_input(self):
         pass
 
     def from_text(self, source, charges, atomic_numbers, mass_numbers, molecular_numbers):
