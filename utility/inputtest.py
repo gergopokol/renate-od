d9a1a47d6abaa4167cf16af237af6c5faa250a2a
asztalosors <asztalos@reak.bme.hu>
2021-11-15 14:20:01 +0100

    #202 Adding basic test classes

---
+++ utility/inputtest.py
@@ -0,0 +1,11 @@
+import os
+import unittest
+from utility.input import AtomicInput, BeamletInput
+
+
+class AtomicInputTest(unittest.TestCase):
+    pass
+
+
+class BeamletIputTest(unittest.TestCase):
+    pass
