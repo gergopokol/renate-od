8fac6b378e5e04a27a5fb53440c9f22cf6dad6fb
asztalosors <asztalos@reak.bme.hu>
2022-01-25 14:41:40 +0100

    #217 Adding scheleton for data fetching from alladin.

---
+++ atomic/alladin_cross_sections.py
@@ -0,0 +1,12 @@
+import requests
+
+
+class AlladinData(object):
+    def __init__(self):
+        pass
+
+    def fetch_data(self, transition):
+        pass
+
+    def check_data(self, transition):
+        pass
