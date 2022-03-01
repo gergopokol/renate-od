bf7a7a8daa7efac2203efea65efdc77a2a38a490
Balazs-Peter <balazs1211@gmail.com>
2022-03-01 17:06:24 +0100

    #186 Modified the rate calculation to provide rates over a range of temperatures working with the new HydrogenicData class.

--- atomic/cross_section.py
+++ atomic/cross_section.py
@@ -49,25 +49,27 @@ class RateCoeff:
         self.transition = transition
         self.crossection = crossection
 
-    def generate_rate(self, temperature, beamenergy):
-        self.temperature = temperature
-        self.beamenergy = beamenergy
-
+    def generate_rates(self, T_range, beamenergy):
         m_t = self.transition.target.mass
-        w = np.sqrt(2*self.temperature*sc.eV/m_t)
-
         m_b = self.transition.projectile.mass
-        v_b = np.sqrt(2*self.beamenergy*sc.eV/m_b)
+        v_b = np.sqrt(2*beamenergy*sc.eV/m_b)
+        rates = np.zeros(len(T_range))
+        for i, T in enumerate(T_range):
+            w = np.sqrt(2*T*sc.eV/m_t)
+            E_range = self.__get_energy_range(T, v_b, w)
+            v = np.sqrt(2*E_range*sc.eV/m_t)
+            crossfunc = self.crossection.get_cross_section(self.transition, E_range)
+            kernel = v**2*crossfunc*(np.exp(-((v-v_b)/w)**2)
+                                     - np.exp(-((v+v_b)/w)**2))/(np.sqrt(np.pi)*w*v_b**2)
+            rates[i] = np.trapz(kernel, v)
+            if self.transition.name == 'de-ex':
+                rates[i] = self.crossection.get_deex_rate(rates[i], T, self.transition)
+        return rates
 
-        E_range = self.crossection.impact_energy
-        v = np.sqrt(2*E_range*sc.eV/m_t)
-        self.velocity = v
-        kernel = v**2*self.crossection.function*(np.exp(-((v-v_b)/w)**2) -
-                                                 np.exp(-((v+v_b)/w)**2))/(np.sqrt(np.pi)*w*v_b**2)
-        self.kernel = kernel
-
-        self.rate = np.trapz(self.kernel, self.velocity)
-        if self.transition.name == 'de-ex':
-            self.rate = self.crossection.atomic_dict['de-ex'](self)
-
-        return self.rate
+    def __get_energy_range(self, T, vb, w, minE=13.6, N=1000, k=3):
+        v_min = max(vb-k*w, 0)
+        v_max = vb+k*w
+        roi_start = max(0.5*self.transition.target.mass*v_min**2/sc.eV, minE)
+        roi_max = 0.5*self.transition.target.mass*v_max**2/sc.eV
+        E_range = np.linspace(roi_start, roi_max, N)
+        return E_range
